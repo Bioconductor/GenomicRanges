d319cb9cd8b73e8645811be647401a5922865616
Martin Morgan <mtmorgan@fhcrc.org>
2010-09-24 16:06:49 +0000

    Add SeqSet class, an eSet-like class for containing highly derived data
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GenomicRanges@49594 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/SeqSet-class.R
+++ R/SeqSet-class.R
@@ -1,9 +1,10 @@
 setClass("SeqSet",
     representation(
         exptData="SimpleList",        # overall description
-        rowData="GRanges",            # rows and their anntoations
+        rowData="GenomicRanges",      # rows and their anntoations
         colData="DataFrame",          # columns and their annotations
-        assays="SimpleList"))         # Data per-se -- e.g., list of matricies
+        assays="SimpleList"),         # Data per-se -- e.g., list of matricies
+    prototype(rowData=GRanges()))
 
 .valid.SeqSet <- function(x)
 {
@@ -19,7 +20,7 @@ setClass("SeqSet",
             next
         }
         edim <- dim(assays[[i]])
-        if (is.null(dim) || !all(xdim == edim))
+        if (is.null(edim) || !all(xdim == edim))
             msg <- c(msg, sprintf(msg2, i))
     }
 
@@ -28,30 +29,97 @@ setClass("SeqSet",
 
 setValidity2("SeqSet", .valid.SeqSet)
 
+SeqSet <-
+    function(assays=SimpleList(), rowData=GRanges(),
+             colData=DataFrame(), exptData=SimpleList(), ...,
+             verbose=FALSE)
+{
+    ## FIXME: warn if dimnames(assays) != list( verbose=TRUE
+    assays <- endoapply(assays, "dimnames<-", NULL)
+    new("SeqSet", exptData=exptData, rowData=rowData, colData=colData,
+        assays=assays, ...)
+}
+
 ## FIXME: MTM thinks that generics do not belong with class definitions
 
 setGeneric("exptData", function(x, ...) standardGeneric("exptData"))
+
+setGeneric("exptData<-",
+    function(x, ..., value) standardGeneric("exptData<-"))
 
 ## rowData, colData seem too vague, but from eSet derived classes wanted to
 ## call the rows / cols something different from 'features' or 'samples', so
 ## might as well avoid the issue
 setGeneric("rowData", function(x, ...) standardGeneric("rowData"))
 
+setGeneric("rowData<-",
+    function(x, ..., value) standardGeneric("rowData<-"))
+
 setGeneric("colData", function(x, ...) standardGeneric("colData"))
+
+setGeneric("colData<-",
+    function(x, ..., value) standardGeneric("colData<-"))
 
 setGeneric("assays", function(x, ...) standardGeneric("assays"))
 
+setGeneric("assays<-",
+    function(x, ..., value) standardGeneric("assays<-"))
+
 setGeneric("assay", function(x, i, ...) standardGeneric("assay"))
 
-## Simple 'getters'
+setGeneric("assay<-",
+    function(x, i, ..., value) standardGeneric("assay<-"))
+
+## Simple 'getters' / 'setters'
 
 setMethod(exptData, "SeqSet", function(x, ...) slot(x, "exptData"))
 
+setReplaceMethod("exptData", c("SeqSet", "SimpleList"),
+    function(x, ..., value)
+{
+    initialize(x, ..., exptData=value)
+})
+
+setReplaceMethod("exptData", c("SeqSet", "list"),
+    function(x, ..., value)
+{
+    initialize(x, ..., exptData=SimpleList(value))
+})
+
 setMethod(rowData, "SeqSet", function(x, ...) slot(x, "rowData"))
+
+setReplaceMethod("rowData", c("SeqSet", "GenomicRanges"),
+    function(x, ..., value)
+{
+    initialize(x, ..., rowData=value)
+})
 
 setMethod(colData, "SeqSet", function(x, ...) slot(x, "colData"))
 
-setMethod(assays, "SeqSet", function(x, ...) slot(x, "assays"))
+setReplaceMethod("colData", c("SeqSet", "DataFrame"),
+    function(x, ..., value)
+{
+    initialize(x, ..., colData=value)
+})
+
+setMethod(assays, "SeqSet",
+    function(x, ...) 
+{
+    endoapply(slot(x, "assays"), "dimnames<-", dimnames(x))
+})
+
+setReplaceMethod("assays", c("SeqSet", "SimpleList"),
+    function(x, ..., value)
+{
+    initialize(x, ..., assays=value)
+})
+
+setReplaceMethod("assays", c("SeqSet", "list"),
+    function(x, ..., value)
+{
+    value <- lapply(x, "dimnames<-", NULL)
+    initialize(x, ..., assays=SimpleList(value))
+})
 
 ## convenience for common use case 
 setMethod(assay, c("SeqSet", "missing"),
@@ -60,7 +128,7 @@ setMethod(assay, c("SeqSet", "missing"),
     assays <- assays(x)
     if (0L == length(assays))
     {
-        msg <- 'assay(x="SeqSet", i="missing", ...) length(assays(x)) is 0'
+        msg <- 'assay(<SeqSet>, i="missing", ...) length(assays(<SeqSet>)) is 0'
         stop(msg)
     }
     assays[[1]]
@@ -69,7 +137,7 @@ setMethod(assay, c("SeqSet", "missing"),
 setMethod(assay, c("SeqSet", "numeric"),
     function(x, i, ...) 
 {
-    msg <- 'assay(x="SeqSet", i="numeric", ...) invalid subscript "i"'
+    msg <- 'assay(<SeqSet>, i="numeric", ...) invalid subscript "i"'
     tryCatch({
         assays(x)[[i]]
     }, error=function(err) {
@@ -80,7 +148,7 @@ setMethod(assay, c("SeqSet", "numeric"),
 setMethod(assay, c("SeqSet", "character"),
     function(x, i = names(x)[1], ...) 
 {
-    msg <- 'assay(x="SeqSet", i="character", ...) invalid subscript "i"'
+    msg <- 'assay(<SeqSet>, i="character", ...) invalid subscript "i"'
     res <-
         tryCatch({
             assays(x)[[i]]
@@ -88,8 +156,34 @@ setMethod(assay, c("SeqSet", "character"),
             stop(msg, "\n", conditionMessage(err))
         })
     if (is.null(res))
-        stop(msg, "\n    not in names(assays(x))")
+        stop(msg, "\n    '", i, "' not in names(assays(<SeqSet>))")
     res
+})
+
+setReplaceMethod("assay", c("SeqSet", "missing", "ANY"),
+    function(x, i, ..., value)
+{
+    if (0L == length(assays(x)))
+    {
+        msg <- "'assay(<SeqSet>) <- value' length(assays(<SeqSet>)) is 0"
+        stop(msg)
+    }
+    assays(x)[[1]] <- value
+    x
+})
+
+setReplaceMethod("assay", c("SeqSet", "numeric", "ANY"),
+    function(x, i = 1, ..., value)
+{
+    assays(x, ...)[[i]] <- value
+    x
+})
+
+setReplaceMethod("assay", c("SeqSet", "character", "ANY"),
+    function(x, i = names(x)[1], ..., value)
+{
+    assays(x, ...)[[i]] <- value
+    x
 })
 
 ## cannonical location for dim, dimnames; dimnames should be checked for
@@ -97,9 +191,141 @@ setMethod(assay, c("SeqSet", "character"),
 ## added from assays if row/col names are NULL in <SeqSet> but not
 ## assays. dimnames need to be added on to assays whne assays() invoked
 setMethod(dim, "SeqSet", function(x) {
-    c(length(rowData(x)), length(colData(x)))
+    c(length(rowData(x)), nrow(colData(x)))
 })
 
 setMethod(dimnames, "SeqSet", function(x) {
-    c(names(rowData(x)), names(colData(x)))
+    list(names(rowData(x)), rownames(colData(x)))
+})
+
+setReplaceMethod("dimnames", c("SeqSet", "list"),
+    function(x, value) 
+{
+    rowData <- rowData(x)
+    names(rowData) <- value[[1]]
+    colData <- colData(x)
+    rownames(colData) <- value[[2]]
+    initialize(x, rowData=rowData, colData=colData)
+})
+        
+setReplaceMethod("dimnames", c("SeqSet", "NULL"),
+    function(x, value)
+{
+    callNextMethod(x, value=list(NULL, NULL))
+})
+
+## Subset -- array-like; [[, $ not defined
+
+.SeqSet.charbound <-
+    function(idx, txt, fmt)
+{
+    orig <- idx
+    idx <- match(idx, txt)
+    if (any(bad <- is.na(idx))) {
+        msg <- paste(selectSome(orig[bad]), collapse=" ")
+        stop(sprintf(fmt, msg))
+    }
+    idx
+}
+
+.SeqSet.subset <-
+    function(x, i, j, ...)
+{
+    if (is.character(i)) {
+        msg <- "<SeqSet>[i,] index out of bounds: %s"
+        i <- .SeqSet.charbound(i, rownames(x), msg)
+    }
+    if (is.character(j)) {
+        msg <- "<SeqSet>[,j] index out of bounds: %s"
+        j <- .SeqSet.charbound(j, colnames(x), msg)
+    }
+    initialize(x, rowData=rowData(x)[i,,drop=FALSE],
+               colData=colData(x)[j,,drop=FALSE],
+               assays=endoapply(assays(x), function(x) {
+                   x <- x[i, j, drop=FALSE]
+                   dimnames(x) <- NULL
+                   x
+               }))
+}
+
+setMethod("[", c("SeqSet", "ANY", "ANY"),
+    function(x, i, j, ..., drop=TRUE)
+{
+    if (1L != length(drop) || (!missing(drop) && drop))
+        warning("'drop' ignored '[,SeqSet,ANY,ANY-method'")
+    if (missing(i) && missing(j))
+        x
+    else if (missing(i))
+        .SeqSet.subset(x, TRUE, j, ...)
+    else if (missing(j))
+        .SeqSet.subset(x, i, TRUE, ...)
+    else
+        .SeqSet.subset(x, i, j, ...)
+})
+
+.SeqSet.subsetassign <-
+    function(x, i, j, ..., value)
+{
+    if (is.character(i)) {
+        msg <- "<SeqSet>[i,]<- index out of bounds: %s"
+        i <- .SeqSet.charbound(i, rownames(x), msg)
+    }
+    if (is.character(j)) {
+        msg <- "<SeqSet>[,j]<- index out of bounds: %s"
+        j <- .SeqSet.charbound(j, colnames(x), msg)
+    }
+    initialize(x,
+               exptData=c(exptData(x), exptData(value)),
+               rowData=local({
+                   r <- rowData(x)
+                   r[i,] <- rowData(value)
+                   r
+               }), colData=local({
+                   c <- colData(x)
+                   c[j,] <- colData(value)
+                   c
+               }), assays=local({
+                   a <- assays(x)
+                   v <- assays(value)
+                   mendoapply(function(x, ..., value) {
+                       x[i,j] <- value
+                       dimnames(x) <- NULL
+                       x
+                   }, x=a, value=v, ...)
+               }))
+}
+
+setReplaceMethod("[", c("SeqSet", "ANY", "ANY", "SeqSet"),
+    function(x, i, j, ..., value)
+{
+    if (missing(i) && missing(j))
+        x
+    else if (missing(i))
+        .SeqSet.subsetassign(x, TRUE, j, ..., value=value)
+    else if (missing(j))
+        .SeqSet.subsetassign(x, i, TRUE, ..., value=value)
+    else
+        .SeqSet.subsetassign(x, i, j, ..., value=value)
+})
+           
+setMethod(show, "SeqSet",
+    function(object)
+{
+    selectSome <- IRanges:::selectSome
+    scat <- function(fmt, vals)
+    {
+        ## FIXME: strwrap
+        txt <- sprintf(fmt, length(vals),
+                       paste(selectSome(vals), collapse=" "))
+        cat(txt)
+    }
+    cat("class:", class(object), "\n")
+    cat("dim:", dim(object), "\n")
+    scat("assays(%d): %s\n", names(assays(object)))
+    dimnames <- dimnames(object)
+    dlen <- sapply(dimnames, length)
+    if (dlen[[1]]) scat("rownames(%d): %s\n", dimnames[[1]])
+    else cat("rownames: NULL\n")
+    if (dlen[[2]]) scat("colnames(%d): %s\n", dimnames[[2]])
+    else cat("colnames: NULL\n")
 })
--- inst/unitTests/test_SeqSet-class.R
+++ inst/unitTests/test_SeqSet-class.R
@@ -1,9 +1,11 @@
-## FIXME: not needed when constructor, generics exported
-assays <- GenomicRanges:::assays
-assay <- GenomicRanges:::assay
-SeqSet <- function(...) new("SeqSet", ...)
+m <- matrix(1, 5, 3, dimnames=list(NULL, NULL))
+assays <- SimpleList(m=m)
+rowData <- GRanges("chr1", IRanges(1:5, 10))
+colData <- DataFrame(x=letters[1:3])
+sset <- SeqSet(assays, rowData, colData)
 
 test_SeqSet_construction <- function() {
+    ## empty-ish
     m1 <- matrix(0, 0, 0)
     checkTrue(validObject(new("SeqSet")))
     checkTrue(validObject(SeqSet()))
@@ -14,14 +16,28 @@ test_SeqSet_construction <- function() {
                    "assays dim mismatch", TRUE)
     checkException(SeqSet(assays=SimpleList(character())),
                    "assays class", TRUE)
+
+    ## substance
+    checkTrue(validObject(sset))
+    checkIdentical(SimpleList(m=m), assays(sset))
+    checkIdentical(GRanges("chr1", IRanges(1:5, 10)), rowData(sset))
+    checkIdentical(DataFrame(x=letters[1:3]), colData(sset))
 }
 
-test_SeqSet_accessors <- function() {
-    m0 <- matrix(0L, 0, 0)
-    m1 <- matrix(0, 0, 0)
-    a <- SimpleList(a=m0, b=m1)
+test_SeqSet_getters <- function() {
+    ## dim, dimnames
+    checkIdentical(c(length(rowData), nrow(colData)), dim(sset))
+    checkIdentical(list(NULL, NULL), dimnames(sset))
+
+    ## row / col / exptData
+    checkIdentical(rowData, rowData(sset))
+    checkIdentical(colData, colData(sset))
+    checkIdentical(SimpleList(), exptData(sset))
 
     ## assays
+    m0 <- matrix(0L, 0, 0, dimnames=list(NULL, NULL))
+    m1 <- matrix(0, 0, 0, dimnames=list(NULL, NULL))
+    a <- SimpleList(a=m0, b=m1)
     checkIdentical(a, assays(SeqSet(assays=a)))
     ## assay
     checkException(assay(SeqSet()), "0-length assay", TRUE)
@@ -30,4 +46,117 @@ test_SeqSet_accessors <- function() {
     checkException(assay(SeqSet(assays=a), 3), "invalid assay index", TRUE)
     checkIdentical(m1, assay(SeqSet(assays=a), "b"), "assay, character index")
     checkException(assay(SeqSet(assays=a), "c"), "invalid assay name", TRUE)
+}
+
+test_SeqSet_setters <- function()
+{
+    ## row / col / exptData<-
+    ss1 <- sset
+    revData <- rowData[rev(seq_len(length(rowData))),,drop=FALSE]
+    rowData(ss1) <- revData
+    checkIdentical(revData, rowData(ss1))
+    checkException(rowData(ss1) <- rowData(sset)[1:2,,drop=FALSE],
+                   "incorrect row dimensions", TRUE)
+    revData <- colData[rev(seq_len(nrow(colData))),,drop=FALSE]
+    colData(ss1) <- revData
+    checkIdentical(revData, colData(ss1))
+    checkException(colData(ss1) <- colData(sset)[1:2,,drop=FALSE],
+                   "incorrect col dimensions", TRUE)
+    lst <- SimpleList("foo", "bar")
+    exptData(ss1) <- lst
+    checkIdentical(lst, exptData(ss1))
+
+    ## assay / assays
+    ss1 <- sset
+    assay(ss1) <- assay(ss1)+1
+    checkIdentical(assay(sset)+1, assay(ss1))
+    ss1 <- sset
+    assay(ss1, 1) <- assay(ss1, 1) + 1
+    checkIdentical(assay(sset, "m") + 1, assay(ss1, "m"))
+    ss1 <- sset
+    assay(ss1, "m") <- assay(ss1, "m") + 1
+    checkIdentical(assay(sset, "m")+1, assay(ss1, "m"))
+
+    ## dimnames<-
+    ss1 <- sset
+    dimnames <- list(letters[seq_len(nrow(ss1))],
+                     LETTERS[seq_len(ncol(ss1))])
+    rownames(ss1) <- dimnames[[1]]
+    colnames(ss1) <- dimnames[[2]]
+    checkIdentical(dimnames, dimnames(ss1))
+    rowData1 <- rowData
+    names(rowData1) <- dimnames[[1]]
+    checkIdentical(rowData1, rowData(ss1))
+    colData1 <- colData
+    row.names(colData1) <- dimnames[[2]]
+    checkIdentical(colData1, colData(ss1))
+    ss1 <- sset
+    dimnames(ss1) <- dimnames
+    checkIdentical(dimnames, dimnames(ss1))
+    dimnames(ss1) <- NULL
+    checkIdentical(list(NULL, NULL), dimnames(ss1))
+}
+
+test_SeqSet_subset <- function()
+{
+    ## numeric
+    ss1 <- sset[2:3,]
+    checkIdentical(c(2L, ncol(sset)), dim(ss1))
+    checkIdentical(rowData(ss1), rowData(sset)[2:3,])
+    checkIdentical(colData(ss1), colData(sset))
+    ss1 <- sset[,2:3]
+    checkIdentical(c(nrow(sset), 2L), dim(ss1))
+    checkIdentical(rowData(ss1), rowData(sset))
+    checkIdentical(colData(ss1), colData(sset)[2:3,,drop=FALSE])
+    ss1 <- sset[2:3, 2:3]
+    checkIdentical(c(2L, 2L), dim(ss1))
+    checkIdentical(rowData(ss1), rowData(sset)[2:3,,drop=FALSE])
+    checkIdentical(colData(ss1), colData(sset)[2:3,,drop=FALSE])
+
+    ## character
+    ss1 <- sset
+    dimnames(ss1) <- list(LETTERS[seq_len(nrow(ss1))],
+                           letters[seq_len(ncol(ss1))])
+    ridx <- c("B", "C")
+    checkIdentical(rowData(ss1[ridx,]), rowData(ss1)[ridx,])
+    checkIdentical(rowData(ss1["D",]), rowData(ss1)["D",,drop=FALSE])
+    checkException(ss1[LETTERS,], "i-index out of bounds", TRUE)
+    cidx <- c("b", "c")
+    checkIdentical(colData(ss1[,cidx]), colData(ss1)[cidx,,drop=FALSE])
+    checkIdentical(colData(ss1[,"a"]), colData(ss1)["a",,drop=FALSE])
+    checkException(ss1[,letters], "j-index out of bounds", TRUE)
+
+    ## logical
+    ss1 <- sset
+    dimnames(ss1) <- list(LETTERS[seq_len(nrow(ss1))],
+                           letters[seq_len(ncol(ss1))])
+    checkIdentical(ss1, ss1[TRUE,])
+    checkIdentical(c(0L, ncol(ss1)), dim(ss1[FALSE,]))
+    checkIdentical(ss1, ss1[,TRUE])
+    checkIdentical(c(nrow(ss1), 0L), dim(ss1[,FALSE]))
+    idx <- c(TRUE, FALSE)               # recycling
+    ss2 <- ss1[idx,]
+    checkIdentical(rowData(ss1)[idx,,drop=FALSE], rowData(ss2))
+    ss2 <- ss1[,idx]
+    checkIdentical(colData(ss1)[idx,,drop=FALSE], colData(ss2))
+}
+
+test_SeqSet_subsetassign <- 
+    function()
+{
+    ss1 <- sset
+    ss1[1:2,] <- ss1[2:1,]
+    checkIdentical(rowData(sset)[2:1,], rowData(ss1)[1:2,])
+    checkIdentical(rowData(sset)[-(1:2),], rowData(ss1)[-(1:2),])
+    checkIdentical(colData(sset), colData(ss1))
+    checkIdentical(c(exptData(sset), exptData(sset)), exptData(ss1))
+
+    ss1 <- sset
+    ss1[,1:2] <- ss1[,2:1,drop=FALSE]
+    checkIdentical(colData(sset)[2:1,,drop=FALSE],
+                   colData(ss1)[1:2,,drop=FALSE])
+    checkIdentical(colData(sset)[-(1:2),,drop=FALSE],
+                   colData(ss1)[-(1:2),,drop=FALSE])
+    checkIdentical(rowData(sset), rowData(ss1))
+    checkIdentical(c(exptData(sset), exptData(sset)), exptData(ss1))
 }
