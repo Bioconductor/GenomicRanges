b5ccd270acaffd4549fb7724a5801111fb2ac447
Herve Pages <hpages@fhcrc.org>
2011-10-12 04:39:52 +0000

    Moved the silly hack to IRanges.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GenomicRanges@59108 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,37 +1,6 @@
 ###
 
-.onLoad <- function(libname, pkgname)
+.onUnload <- function(libpath)
 {
-    ## Putting library(methods) below produces the following 'R CMD check'
-    ## NOTE:
-    ##
-    ##   * checking R code for possible problems ... NOTE
-    ##   File ‘GenomicRanges/R/zzz.R’:
-    ##     .onLoad calls:
-    ##       library(methods)
-    ##
-    ##   Package startup functions should not change the search path.
-    ##   See section ‘Good practice’ in ?.onAttach.
-    ##
-    ## On the other hand, NOT loading the methods package will cause the
-    ## following WARNING when running 'R CMD check' on a BSgenome data package:
-    ##
-    ##   * checking whether the namespace can be loaded with stated dependencies ... WARNING
-    ##   Error: .onLoad failed in loadNamespace() for ‘BSgenome.Scerevisiae.UCSC.sacCer3’, details:
-    ##     call: length(x)
-    ##     error: could not find function "loadMethod"
-    ##   Execution halted
-    ##
-    ##   A namespace must be able to be loaded with just the base namespace
-    ##   loaded: otherwise if the namespace gets loaded by a saved object, the
-    ##   session will be unable to start.
-    ##
-    ##   Probably some imports need to be declared in the NAMESPACE file.
-    ##
-    ## Hence the following trick...
-    sillyname <- library  # cheating on codetools
-    sillyname(methods)
+    library.dynam.unload("GenomicRanges", libpath)
 }
-
-.onUnload <- function(libpath) library.dynam.unload("GenomicRanges", libpath)
-
