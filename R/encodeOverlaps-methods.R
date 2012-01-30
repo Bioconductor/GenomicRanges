f1cc6eaf48344958ac585403238eac3c4dba45f4
Herve Pages <hpages@fhcrc.org>
2012-01-30 18:58:34 +0000

    Replace readIsCompatibleWithSplicing() regular function by isCompatibleWithSplicing() generic.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GenomicRanges@62384 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/encodeOverlaps-methods.R
+++ R/encodeOverlaps-methods.R
@@ -53,22 +53,46 @@ setMethod("encodeOverlaps", c("GRangesList", "GRangesList"),
 ### Related utilities.
 ###
 
-readIsCompatibleWithSplicing <- function(x)
+setGeneric("isCompatibleWithSplicing",
+    function(x) standardGeneric("isCompatibleWithSplicing")
+)
+
+.whichCompatibleWithSplicing <- function(x)
 {
-    if (!is(x, "OverlapEncodings"))
-        stop("'x' must be an OverlapEncodings object")
-    if (length(x) == 0L)
-        return(logical(0))
-    ngap <- as.integer(sub(":.*", "", levels(encoding(x)))) - 1L
-    if (max(ngap) > 2)
-        stop("don't support reads with more than 2 gaps for now, sorry")
+    ngap <- as.integer(sub(":.*", "", x)) - 1L
+    if (max(ngap) > 3)
+        stop("don't support reads with more than 3 gaps for now, sorry")
     ## Regex to use for reads with 1 range (no gaps):
     pattern1 <- ":[fgij]:"
     ## Regex to use for reads with 2 ranges (1 gap):
     pattern2 <- ":[jg].:.[gf]:"
     ## Regex to use for reads with 3 ranges (2 gaps):
     pattern3 <- ":[jg]..:.g.:..[gf]:"
-    pattern123 <- ":([fgij]|[jg].:.[gf]|[jg]..:.g.:..[gf]):"
-    as.integer(encoding(x)) %in% grep(pattern123, levels(encoding(x)))
+    ## Regex to use for reads with 4 ranges (3 gaps):
+    pattern4 <- ":[jg]...:.g..:..g.:...[gf]:"
+    pattern1234 <- ":([fgij]|[jg].:.[gf]|[jg]..:.g.:..[gf]|[jg]...:.g..:..g.:...[gf]):"
+    grep(pattern1234, x)
 }
+
+setMethod("isCompatibleWithSplicing", "character",
+    function(x)
+    {
+        stop("IMPLEMENT ME")
+    }
+)
+
+setMethod("isCompatibleWithSplicing", "factor",
+    function(x)
+    {
+        if (length(x) == 0L)
+            return(logical(0))
+        idx <- .whichCompatibleWithSplicing(levels(x))
+        as.integer(x) %in% idx
+    }
+)
+
+setMethod("isCompatibleWithSplicing", "OverlapEncodings",
+    function(x)
+        isCompatibleWithSplicing(encoding(x))
+)
 
