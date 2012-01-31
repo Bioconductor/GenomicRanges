58e0aad72da5c1bf568a207cd77fb52a2dd43f00
Michael Lawrence <lawremi@iastate.edu>
2012-01-31 22:42:22 +0000

    Add seqinfo[<-] methods for List (assuming one sequence per list element). And seqnames methods for RangesList and RangedData. Tries to make IRanges structures more GenomicRanges friendly.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GenomicRanges@62427 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/RangedData-methods.R
@@ -0,0 +1,51 @@
+### =========================================================================
+### RangedData/RangesList implementation of the GenomicRanges API
+### -------------------------------------------------------------------------
+###
+
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+### seqinfo
+###
+
+setMethod("seqinfo", "List", function(x) {
+  si <- metadata(x)$seqinfo
+  if (is.null(si)) {
+    sn <- names(x)
+    if (is.null(sn))
+      sn <- as.character(seq(length(x)))
+    si <- Seqinfo(sn)
+  }
+  si  
+})
+
+setMethod("seqinfo", "RangesList", function(x) {
+  si <- callNextMethod()
+  if (!is.null(universe(x)))
+    genome(si) <- universe(x)
+  si
+})
+
+### FIXME: needs sanity checks
+setReplaceMethod("seqinfo", "List",
+                 function(x, value) {
+                   metadata(x)$seqinfo <- value
+                   x
+                 })
+
+setMethod("seqinfo", "RangedData", function(x) seqinfo(ranges(x)))
+setReplaceMethod("seqinfo", "RangedData",
+                 function(x, value) {
+                   seqinfo(ranges(x)) <- value
+                   x
+                 })
+
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+### seqnames
+###
+
+setMethod("seqnames", "RangesList", function(x) {
+  seqsplit(Rle(space(x)), rep(names(x), elementLengths(x)))
+})
+
+setMethod("seqnames", "RangedData", function(x) space(x))
+
