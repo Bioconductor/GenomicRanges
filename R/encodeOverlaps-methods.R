f6396f911558c358260eea6114a1cc24bfe755db
Herve Pages <hpages@fhcrc.org>
2012-05-15 21:59:01 +0000

    Add high-level convenience wrappers findCompatibleOverlaps() and countCompatibleOverlaps().
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GenomicRanges@65929 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/encodeOverlaps-methods.R
+++ R/encodeOverlaps-methods.R
@@ -751,3 +751,40 @@ extractQueryStartInTranscript <- function(query, subject,
                check.names=FALSE, stringsAsFactors=FALSE)
 }
 
+
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+### High-level convenience wrappers: findCompatibleOverlaps() and
+### countCompatibleOverlaps().
+###
+
+setGeneric("findCompatibleOverlaps",
+    function(query, subject) standardGeneric("findCompatibleOverlaps")
+)
+
+.GappedAlignmentsORGappedAlignmentPairs.findCompatibleOverlaps <-
+    function(query, subject)
+{
+    grl <- grglist(query, order.as.in.query=TRUE)
+    ## TODO: Use 'type="within"' when it's supported for circular
+    ## sequences like the mitochondrial chromosome.
+    ov <- findOverlaps(grl, subject, ignore.strand=TRUE)
+    ovenc <- encodeOverlaps(grl, subject, hits=ov,
+                            flip.query.if.wrong.strand=TRUE)
+    ov_is_compat <- isCompatibleWithSplicing(ovenc)
+    ov[ov_is_compat]
+}
+
+setMethod("findCompatibleOverlaps", c("GappedAlignments", "GRangesList"),
+    .GappedAlignmentsORGappedAlignmentPairs.findCompatibleOverlaps
+)
+
+setMethod("findCompatibleOverlaps", c("GappedAlignmentPairs", "GRangesList"),
+    .GappedAlignmentsORGappedAlignmentPairs.findCompatibleOverlaps
+)
+
+countCompatibleOverlaps <- function(query, subject)
+{
+    compatov <- findCompatibleOverlaps(query, subject)
+    tabulate(queryHits(compatov), nbins=queryLength(compatov))
+}
+
