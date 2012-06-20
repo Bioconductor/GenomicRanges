32e9bce6115e6590946a2ef2cfe86b122fada6b7
Herve Pages <hpages@fhcrc.org>
2012-06-20 00:22:15 +0000

    Add phicoef() (will be needed for "Overlap encodings" vignette).
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GenomicRanges@66839 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/phicoef.R
@@ -0,0 +1,29 @@
+### Based on http://en.wikipedia.org/wiki/Phi_coefficient
+
+phicoef <- function(x, y=NULL)
+{
+    if (is.null(y)) {
+        if (!is.integer(x) || length(x) != 4L)
+            stop("when 'y' is not supplied, 'x' must be a 2x2 integer matrix ",
+                 "or a vector of length 4")
+        a <- x[1L]
+        c <- x[2L]
+        b <- x[3L]
+        d <- x[4L]
+    } else {
+        if (!is.logical(x) || !is.logical(y) || length(x) != length(y))
+            stop("when 'y' is supplied, 'x' and 'y' must be ",
+                 "2 logical vectors of the same length")
+        a <- sum(x  &  y)
+        b <- sum(x  & !y)
+        c <- sum(!x &  y)
+        d <- sum(!x & !y)
+    }
+    a <- as.double(a)
+    b <- as.double(b)
+    c <- as.double(c)
+    d <- as.double(d)
+    div <- sqrt((a + b) * (c + d) * (a + c) * (b + d))
+    (a * d - b * c) / div
+}
+
