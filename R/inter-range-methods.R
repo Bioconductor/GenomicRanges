ec4f03ac13909dc5b0457bac8aeccc09a142be41
Herve Pages <hpages@fhcrc.org>
2013-01-17 06:32:43 +0000

    Implement 'with.mapping' arg for "reduce" method for GenomicRanges objects.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GenomicRanges@72609 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/inter-range-methods.R
+++ R/inter-range-methods.R
@@ -4,10 +4,6 @@
 ###
 
 
-### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-### range()
-###
-
 .interIntervalSplitVariable <- function(x, ignore.strand=FALSE)
 {
     f1 <- seqnames(x)
@@ -19,13 +15,35 @@
     f1 + f2
 }
 
-.interIntervalGenomicRanges2 <- function(x, FUN, ignore.strand=FALSE, ...)
+### 'mapping_unlisted' and 'mapping_partitioning': IntegerList and Partitioning
+### objects representing the "mapping object", which is *conceptually* an
+### IntegerListList object (of the same length as 'mapping_partitioning').
+### *Conceptually* because, well, we don't actually have such container...
+### 'old2new': IntegerList of the same length as the "mapping object".
+.translateMapping <- function(mapping_unlisted, mapping_partitioning, old2new)
+{
+    ## 'times' has the length of the "mapping object".
+    times <- sum(relist(width(PartitioningByEnd(mapping_unlisted)),
+                        mapping_partitioning))
+    ## 'offset' has the length of 'mapping_unlisted@unlistData'.
+    offset <- rep.int(start(PartitioningByEnd(old2new)) - 1L, times)
+    mapping_flat <- mapping_unlisted@unlistData
+    mapping_unlisted@unlistData <- old2new@unlistData[mapping_flat + offset]
+    mapping_unlisted
+}
+
+.interIntervalGenomicRanges2 <- function(x, FUN, with.mapping=FALSE,
+                                         ignore.strand=FALSE, ...)
 {
     x <- clone(x)
     mcols(x) <- NULL
     f <- .interIntervalSplitVariable(x, ignore.strand=ignore.strand)
     xIRangesList <- split(unname(ranges(x)), f)
-    ansIRangesList <- FUN(xIRangesList, ...)
+    if (with.mapping) {
+        ansIRangesList <- FUN(xIRangesList, with.mapping=TRUE, ...)
+    } else {
+        ansIRangesList <- FUN(xIRangesList, ...)
+    }
     k <- elementLengths(ansIRangesList)
     x_seqlevels <- seqlevels(x)
     strand_levels <- levels(strand())
@@ -38,11 +56,32 @@
         i2 <- ansIRangesList_names %% 3L + 1L
         ans_strand <- Rle(factor(strand_levels[i2], levels=strand_levels), k)
     }
+    ans_ranges <- unlist(ansIRangesList, use.names=FALSE)
+    ans_mcols <- mcols(ans_ranges)
+    if (is.null(ans_mcols)) {
+        ans_mcols <- new("DataFrame", nrows=length(ans_seqnames))
+    } else {
+        mcols(ans_ranges) <- NULL
+        if (with.mapping) {
+            mapping_unlisted <- ans_mcols[["mapping"]]
+            mapping_partitioning <- PartitioningByEnd(ansIRangesList)
+            old2new <- splitAsList(seq_along(x), f)
+            mapping_unlisted2 <- .translateMapping(mapping_unlisted,
+                                                   mapping_partitioning,
+                                                   old2new)
+            ans_mcols[["mapping"]] <- mapping_unlisted2
+        }
+    }
     update(x, seqnames=ans_seqnames,
-           ranges=unlist(ansIRangesList, use.names=FALSE),
-           strand=ans_strand,
-           elementMetadata=new("DataFrame", nrows=length(ans_seqnames)))
+              ranges=ans_ranges,
+              strand=ans_strand,
+              elementMetadata=ans_mcols)
 }
+
+
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+### range()
+###
 
 setMethod("range", "GenomicRanges",
     function(x, ..., ignore.strand=FALSE, na.rm=FALSE)
@@ -57,7 +96,7 @@ setMethod("range", "GenomicRanges",
 
 setMethod("reduce", "GenomicRanges",
     function(x, drop.empty.ranges=FALSE, min.gapwidth=1L,
-             with.inframe.attrib=FALSE, ignore.strand=FALSE)
+             with.mapping=FALSE, with.inframe.attrib=FALSE, ignore.strand=FALSE)
     {
         if (!identical(with.inframe.attrib, FALSE))
             stop("'with.inframe.attrib' argument not supported ",
@@ -68,7 +107,8 @@ setMethod("reduce", "GenomicRanges",
             strand(x) <- "*"
         .interIntervalGenomicRanges2(x, reduce,
                                      drop.empty.ranges=drop.empty.ranges,
-                                     min.gapwidth=min.gapwidth)
+                                     min.gapwidth=min.gapwidth,
+                                     with.mapping=with.mapping)
     }
 )
 
