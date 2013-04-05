Bioconductor/GenomicRanges

commit 43c2e2399a1ce9c96d67b3b6423b6b6b13f85efd

Tweak to GRanges() constructor so the names of the supplied metadata cols don't run in conflict with some obscure internal argument names.
git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GenomicRanges@73720 bc3139a8-67e5-0310-9ffc-ced21a209358

DESCRIPTION
@@ -6,7 +6,7 @@ Description: The ability to efficiently store genomic annotations and
 	defines general purpose containers for storing genomic intervals
 	as well as more specialized containers for storing alignments
 	against a reference genome.
-Version: 1.11.30
+Version: 1.11.31
 Author: P. Aboyoun, H. Pages and M. Lawrence
 Maintainer: Bioconductor Package Maintainer <maintainer@bioconductor.org>
 biocViews: Genetics, Sequencing, HighThroughputSequencing, Annotation

R/GRanges-class.R
@@ -55,11 +55,8 @@ setValidity2("GRanges", .valid.GRanges)
 newGRanges <- ## hidden constructor shared with other GRanges-like objects
     function(class, seqnames = Rle(), ranges = IRanges(),
              strand = Rle("*", length(seqnames)),
-             ...,
-             seqlengths = setNames(
-               rep(NA_integer_, length(levels(seqnames))),
-               levels(seqnames)),
-             seqinfo = Seqinfo(names(seqlengths), seqlengths))
+             mcols = list(),
+             seqlengths = NULL, seqinfo = NULL)
 {
     ## occurs first for generation of default seqlengths
     if (!is(seqnames, "Rle"))
@@ -90,6 +87,13 @@ newGRanges <- ## hidden constructor shared with other GRanges-like objects
             strand <- rep(strand, lx)
     }
 
+    if (is.null(seqlengths))
+        seqlengths <- setNames(rep(NA_integer_, length(levels(seqnames))),
+                               levels(seqnames))
+
+    if (is.null(seqinfo))
+        seqinfo <- Seqinfo(names(seqlengths), seqlengths)
+
     ## in case we have seqlengths for unrepresented sequences
     runValue(seqnames) <- factor(runValue(seqnames), seqnames(seqinfo))
 
@@ -97,7 +101,7 @@ newGRanges <- ## hidden constructor shared with other GRanges-like objects
         warning("'ranges' has metadata columns, dropping them")
         mcols(ranges) <- NULL
     }
-    mcols <- DataFrame(...)
+    mcols <- as(mcols, "DataFrame")
     if (ncol(mcols) == 0L)
         mcols <- new("DataFrame", nrows = length(seqnames))
     if (!is.null(rownames(mcols))) {
@@ -113,15 +117,14 @@ newGRanges <- ## hidden constructor shared with other GRanges-like objects
 GRanges <-
   function(seqnames = Rle(), ranges = IRanges(),
            strand = Rle("*", length(seqnames)),
-           ..., seqinfo)
+           ...,
+           seqlengths = NULL, seqinfo = NULL)
 {
-    if (missing(seqinfo)) {
-        newGRanges("GRanges", seqnames = seqnames, ranges = ranges,
-                   strand = strand, ...)
-    } else {
-        newGRanges("GRanges", seqnames = seqnames, ranges = ranges,
-                   strand = strand, ..., seqinfo = seqinfo)
-    }
+    newGRanges("GRanges", seqnames = seqnames, ranges = ranges,
+                          strand = strand,
+                          mcols = list(...),
+                          seqlengths = seqlengths,
+                          seqinfo = seqinfo)
 }
 
 .unsafe.update.GRanges <- function(x, ...)

man/GRanges-class.Rd
@@ -98,7 +98,8 @@
     \item{}{
       \code{GRanges(seqnames = Rle(), ranges = IRanges(),
                     strand = Rle("*", length(seqnames)),
-                    ..., seqinfo)}:
+                    ...,
+                    seqlengths = NULL, seqinfo = NULL)}:
       Creates a GRanges object.
       \describe{
         \item{\code{seqnames}}{Rle object, character vector, or factor
@@ -106,14 +107,17 @@
         \item{\code{ranges}}{IRanges object containing the ranges.}
         \item{\code{strand}}{Rle object, character vector, or factor
           containing the strand information.}
-        \item{\code{seqinfo}}{a Seqinfo object containing allowed
-          sequence names and lengths (or NA) for each
-          \code{level(seqnames)}.}
         \item{\code{\ldots}}{Optional metadata columns.
           These columns cannot be named
           \code{"start"}, \code{"end"}, \code{"width"}, or
           \code{"element"}. A named integer vector \code{"seqlength"}
 	  can be used instead of \code{seqinfo}.}
+        \item{\code{seqlengths}}{an integer vector named with the
+          sequence names and containing the lengths (or NA) for each
+          \code{level(seqnames)}.}
+        \item{\code{seqinfo}}{a Seqinfo object containing allowed
+          sequence names and lengths (or NA) for each
+          \code{level(seqnames)}.}
       }
     }
   }

commit 01b2d89856796a022d0796a17cadfd57be65ce84

Fixed regression (introduced in r73720) in GRanges() constructor when passing unnamed metadata cols.
git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GenomicRanges@75394 bc3139a8-67e5-0310-9ffc-ced21a209358

DESCRIPTION
@@ -6,7 +6,7 @@ Description: The ability to efficiently store genomic annotations and
 	defines general purpose containers for storing genomic intervals
 	as well as more specialized containers for storing alignments
 	against a reference genome.
-Version: 1.13.0
+Version: 1.13.1
 Author: P. Aboyoun, H. Pages and M. Lawrence
 Maintainer: Bioconductor Package Maintainer <maintainer@bioconductor.org>
 biocViews: Genetics, Sequencing, HighThroughputSequencing, Annotation

R/GRanges-class.R
@@ -55,7 +55,7 @@ setValidity2("GRanges", .valid.GRanges)
 newGRanges <- ## hidden constructor shared with other GRanges-like objects
     function(class, seqnames = Rle(), ranges = IRanges(),
              strand = Rle("*", length(seqnames)),
-             mcols = list(),
+             mcols = DataFrame(),
              seqlengths = NULL, seqinfo = NULL)
 {
     ## occurs first for generation of default seqlengths
@@ -101,11 +101,13 @@ newGRanges <- ## hidden constructor shared with other GRanges-like objects
         warning("'ranges' has metadata columns, dropping them")
         mcols(ranges) <- NULL
     }
-    mcols <- as(mcols, "DataFrame")
-    if (ncol(mcols) == 0L)
+
+    if (!is(mcols, "DataFrame"))  # should never happen when calling GRanges()
+        stop("'mcols' must be a DataFrame object")
+    if (ncol(mcols) == 0L) {
         mcols <- new("DataFrame", nrows = length(seqnames))
-    if (!is.null(rownames(mcols))) {
-        if (!is.null(names(ranges)))
+    } else if (!is.null(rownames(mcols))) {
+        if (is.null(names(ranges)))
             names(ranges) <- rownames(mcols)
         rownames(mcols) <- NULL
     }
@@ -122,7 +124,7 @@ GRanges <-
 {
     newGRanges("GRanges", seqnames = seqnames, ranges = ranges,
                           strand = strand,
-                          mcols = list(...),
+                          mcols = DataFrame(...),
                           seqlengths = seqlengths,
                           seqinfo = seqinfo)
 }