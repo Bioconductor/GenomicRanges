c9d7fa12c26c045001e1ab891997fac481956c52
Herve Pages <hpages@fhcrc.org>
2013-05-08 21:08:57 +0000

    50x speedup or more when merging 2 Seqinfo objects, 1 very small and 1 very big
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GenomicRanges@76301 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/utils.R
+++ R/utils.R
@@ -150,13 +150,19 @@ mergeNamedAtomicVectors <- function(x, y, what=c("key", "values"))
     ans_names <- union(x_names, y_names)
     if (any(ans_names %in% c(NA_character_, "")))
         stop("some names in 'x' or 'y' are NA or the empty string")
-    ans <- x[ans_names]
+    ## Note sure why but subsetting by name is *very* slow when the character
+    ## vector used as subscript contains a lot of "invalid" names. I already
+    ## reported this issue twice on the R-devel mailing list (in July 2010 and
+    ## May 2013) with no answer so far.
+    #ans <- x[ans_names]  # very slow :-(
+    ans <- x[match(ans_names, x_names)]  # much faster :-)
     ## Some of 'ans' names can be NA. This is because subsetting a named
     ## vector 'x' by a character vector 'i' returns a vector whose names
     ## are 'i' except for the values in 'i' that are not in 'names(x)'.
     ## So we need to fix this.
     names(ans) <- ans_names
-    ans2 <- y[ans_names]
+    #ans2 <- y[ans_names]  # very slow :-(
+    ans2 <- y[match(ans_names, y_names)]  # much faster :-)
     idx <- which(ans != ans2)
     if (length(idx) != 0L) {
         msg <- c(what[1L], ifelse(length(idx) >= 2, "s", ""), " ",
