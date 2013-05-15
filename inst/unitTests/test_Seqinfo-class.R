39eceb362961d6d7be287185deec07c1f2508a7f
Valerie Obenchain <vobencha@fhcrc.org>
2013-05-15 23:21:55 +0000

    Moved unit tests for keepSeqlevels() and renameSeqlevels() to test_Seqinfo-class.R; removed test_Seqinfo-class.R.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GenomicRanges@76635 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/unitTests/test_Seqinfo-class.R
+++ inst/unitTests/test_Seqinfo-class.R
@@ -1,4 +1,65 @@
-###
+gr1 <- GRanges(c("chr1", "chr1"), IRanges(1, 5))
+gr2 <- GRanges(c("chr1", "chr2"), IRanges(1, 5))
+gr3 <- GRanges(c("chr3", "chr3", "chr3"), IRanges(1:3, 5))
+
+test_Seqinfo_seqlevels_subset <- function()
+{
+    ## GRanges
+    gr <- gr2
+    seqlevels(gr, force=TRUE) <- "chr2"
+    checkIdentical("chr2", seqlevels(gr))
+    checkIdentical(1L, length(gr))
+    gr <- suppressWarnings(c(gr1, gr2, gr3))
+
+    ## GRangesList
+    grl <- GRangesList(gr1, gr2, gr3)
+    seqlevels(grl, force=TRUE) <- "chr1"
+    checkIdentical("chr1", seqlevels(grl))
+    checkIdentical(1L, length(grl))
+
+    grl <- GRangesList(gr2)
+    seqlevels(grl, force=TRUE) <- "chr1"
+    checkIdentical(0L, length(grl)) ## warning?
+
+    ## GAlignments
+    gal <- GAlignments(seqnames=Rle(c("chr1", "chr2")),
+                        pos=as.integer(c(10, 100)),
+                        cigar=c("50M", "50M"),
+                        strand=strand(c("*", "*")))
+    seqlevels(gal, force=TRUE) <- "chr2"
+    checkIdentical("chr2", seqlevels(gal))
+}
+
+test_Seqinfo_seqlevels_rename <- function()
+{
+    ## GRanges
+    gr <- c(gr1, gr2, gr3) 
+    seqlevels(gr) <- gsub("chr", "CHR", seqlevels(gr))
+    checkIdentical(c("CHR1", "CHR2", "CHR3"), seqlevels(gr))
+    seqlevels(gr)[seqlevels(gr) == "CHR2"] <- "2"
+    checkIdentical(c("CHR1", "2", "CHR3"), seqlevels(gr))
+    #checkException(seqlevels(gr) <- rev(seqlevels(gr))) ## exception?
+    #nms <- seqlevels(gr)
+    #new <- rev(seqlevels(gr))
+    #names(new) <- nms
+    #checkException(seqlevels(gr) <- rev(seqlevels(gr))) ## exception?
+
+    ## GRangesList
+    grl <- GRangesList(gr1, gr2, gr3)
+    #seqlevels(grl)[seqlevels(grl) == "chr2"] <- "chr1" ## should work?
+    #seqlevels(grl)[seqlevels(grl) == "chr3"] <- "chr1" ## should work?
+    seqlevels(grl)[seqlevels(grl) == "chr3"] <- "3"
+    checkIdentical(c("chr1", "chr2", "3"), seqlevels(grl))
+
+
+    ## GAlignments
+    gal <- GAlignments(seqnames=Rle(c("chr1", "chr2")),
+                        pos=as.integer(c(10, 100)),
+                        cigar=c("50M", "50M"),
+                        strand=strand(c("*", "*")))
+    seqlevels(gal)[seqlevels(gal) == "chr2"] <- "2"
+    checkIdentical(c("chr1", "2"),  seqlevels(gal))
+}
 
 test_Seqinfo.merge <- function()
 {
