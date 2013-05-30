f04260632058ed9ed23052c765195e7e18c9c6e7
Herve Pages <hpages@fhcrc.org>
2013-05-30 22:56:20 +0000

    Document explodeCigarOps() and explodeCigarOpLengths().
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GenomicRanges@77057 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/cigar_utils.c
+++ src/cigar_utils.c
@@ -613,7 +613,7 @@ SEXP valid_cigar(SEXP cigar, SEXP ans_type)
 
 
 /****************************************************************************
- * --- .Call ENTRY POINT ---
+ * --- .Call ENTRY POINTS ---
  *   - explode_cigar_ops()
  *   - explode_cigar_op_lengths()
  * Arg:
@@ -621,12 +621,12 @@ SEXP valid_cigar(SEXP cigar, SEXP ans_type)
  *          explode.
  * Both functions return a list of the same length as 'cigar' where each
  * list element is a character vector (for explode_cigar_ops()) or an integer
- * vector (for explode_cigar_op_lengths()). The 2 lists have the same shape
- * i.e. same length() and same elementLengths(). The i-th character vector in
- * the list returned by explode_cigar_ops() contains one single-letter string
- * per CIGAR operation in 'cigar[i]'. The i-th integer vector in the list
- * returned by explode_cigar_op_lengths() contains the corresponding CIGAR
- * operation lengths. Zero-length operations are ignored.
+ * vector (for explode_cigar_op_lengths()). The 2 lists have the same shape,
+ * that is, same length() and same elementLengths(). The i-th character vector
+ * in the list returned by explode_cigar_ops() contains one single-letter
+ * string per CIGAR operation in 'cigar[i]'. The i-th integer vector in the
+ * list returned by explode_cigar_op_lengths() contains the corresponding
+ * CIGAR operation lengths. Zero-length operations are ignored.
  */
 SEXP explode_cigar_ops(SEXP cigar)
 {
