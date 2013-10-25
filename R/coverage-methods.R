571e6a9d31933041610b2cbd05fd969192168f38
Herve Pages <hpages@fhcrc.org>
2013-10-25 09:43:17 +0000

    Hopefully the final pass on coverage(): - Fix some regressions introduced in the last few days in "coverage" method for   GenomicRanges objects. - Improve man page. - Improve formatting of unit tests.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GenomicRanges@82149 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/coverage-methods.R
+++ R/coverage-methods.R
@@ -3,22 +3,72 @@
 ### -------------------------------------------------------------------------
 ###
 
+### TODO: Merge with Biostrings:::.V_recycle() and put in IRanges.
+.recycle <- function(x, skeleton_len, x.label, skeleton.label)
+{
+    x_len <- length(x)
+    if (x_len == skeleton_len)
+        return(x)
+    if (x_len < skeleton_len) {
+        if (x_len == 0L)
+            stop("cannot recycle zero-length '", x.label, "' ",
+                 "to the length of '", skeleton.label, "'")
+    } else {
+        if (x_len >= 2L)
+            stop("'", x.label, "' is longer than '", skeleton.label, "'")
+    }
+    if (skeleton_len %% x_len != 0L)
+        warning("'", x.label, "' length is not a divisor ",
+                "of '", skeleton.label, "' length")
+    rep(x, length.out=skeleton_len)
+}
+
+.normarg_shift_or_weight <- function(arg, arg.label, x)
+{
+    if (is.list(arg) || is(arg, "List")) {
+        if (!identical(names(arg), seqlevels(x)))
+            stop("when '", arg.label, "' is a list-like object, it must ",
+                 "have 1 list element per seqlevel in 'x', and its names ",
+                 "must be exactly 'seqlevels(x)'")
+        return(arg)
+    }
+    if (isSingleString(arg)) {
+        x_mcols <- mcols(x)
+        if (!is(x_mcols, "DataTable")
+         || sum(colnames(x_mcols) == arg) != 1L)
+            stop("'mcols(x)' has 0 or more than 1 \"",
+                 arg, "\" columns")
+        arg <- x_mcols[ , arg]
+    }
+    if (!is.numeric(arg))
+        stop("'", arg.label, "' must be a numeric vector, a single string, ", 
+             "or a list-like object")
+    split(.recycle(arg, length(x), arg.label, "x"), seqnames(x))
+}
+
 setMethod("coverage", "GenomicRanges",
     function(x, shift=0L, width=NULL, weight=1L,
                 method=c("auto", "sort", "hash"))
     {
-        if (isSingleString(weight)) {
-            x_mcols <- mcols(x)
-            if (!is(x_mcols, "DataTable")
-             || sum(colnames(x_mcols) == weight) != 1L)
-                stop("'mcols(x)' has 0 or more than 1 \"",
-                     weight, "\" columns")
-            weight <- x_mcols[[weight]]
+        ## Normalize 'shift'.
+        shift <- .normarg_shift_or_weight(shift, "shift", x)
+
+        ## Normalize 'width'.
+        if (is.null(width)) {
+            width <- seqlengths(x)
+        } else if (!(is.numeric(width)
+                  && identical(names(width), seqlevels(x)))) {
+            stop("'width' must be NULL or an integer vector with ",
+                 "the length and names of 'seqlengths(x)'")
         }
-        y <- split(ranges(x), seqnames(x))
+
+        ## Normalize 'weight'.
+        weight <- .normarg_shift_or_weight(weight, "weight", x)
+
+        x_ranges_list <- split(ranges(x), seqnames(x))
         circle.length <- seqlengths(x)
         circle.length[!(isCircular(x) %in% TRUE)] <- NA_integer_
-        IRanges:::.CompressedIRangesList.coverage(y,
+        IRanges:::.CompressedIRangesList.coverage(x_ranges_list,
                                         shift=shift,
                                         width=width,
                                         weight=weight,
@@ -28,21 +78,25 @@ setMethod("coverage", "GenomicRanges",
 )
 
 setMethod("coverage", "GRangesList",
-    function(x, shift=0L, width=NULL, weight=1L, ...)
+    function(x, shift=0L, width=NULL, weight=1L,
+                method=c("auto", "sort", "hash"))
     {
-        coverage(x@unlistData, shift=shift, width=width, weight=weight, ...)
+        coverage(x@unlistData, shift=shift, width=width, weight=weight,
+                 method=method)
     }
 )
 
 setMethod("coverage", "GAlignments",
-    function(x, shift=0L, width=NULL, weight=1L, drop.D.ranges = FALSE, ...)
-        coverage(grglist(x, drop.D.ranges = drop.D.ranges),
-                 shift=shift, width=width, weight=weight, ...)
+    function(x, shift=0L, width=NULL, weight=1L,
+                method=c("auto", "sort", "hash"), drop.D.ranges=FALSE)
+        coverage(grglist(x, drop.D.ranges=drop.D.ranges),
+                 shift=shift, width=width, weight=weight, method=method)
 )
 
 setMethod("coverage", "GAlignmentPairs",
-    function(x, shift=0L, width=NULL, weight=1L, drop.D.ranges = FALSE, ...)
-          coverage(grglist(x, drop.D.ranges = drop.D.ranges),
-                   shift=shift, width=width, weight=weight, ...)
+    function(x, shift=0L, width=NULL, weight=1L,
+                method=c("auto", "sort", "hash"), drop.D.ranges=FALSE)
+        coverage(grglist(x, drop.D.ranges=drop.D.ranges),
+                 shift=shift, width=width, weight=weight, method=method)
 )
 
--- inst/unitTests/test_coverage-methods.R
+++ inst/unitTests/test_coverage-methods.R
@@ -27,26 +27,33 @@ make_test_GRangesList <- function() {
 
 test_coverage_GRanges <- function() {
     gr <- make_test_GRanges()
-    checkIdentical(coverage(gr),
-                   RleList("chr1" = Rle(1:3, c(4, 1, 5)),
-                           "chr2" = Rle(0:3, c(1, 1, 1, 7)),
-                           "chr3" = Rle(0:4, c(6, 1, 1, 1, 1)),
-                           compress=FALSE))
-    checkIdentical(coverage(gr, width = c(10, 20, 30)),
-                   RleList("chr1" = Rle(1:3, c(4, 1, 5)),
-                           "chr2" = Rle(c(0:3, 0L), c(1, 1, 1, 7, 10)),
-                           "chr3" = Rle(c(0:4, 0L), c(6, 1, 1, 1, 1, 20)),
-                           compress=FALSE))
-    checkIdentical(coverage(gr, weight = list(1L, 10L, 100L)),
-                   RleList("chr1" = Rle(1:3, c(4, 1, 5)),
-                           "chr2" = Rle(10L * 0:3, c(1, 1, 1, 7)),
-                           "chr3" = Rle(100L * 0:4, c(6, 1, 1, 1, 1)),
-                           compress=FALSE))
-    checkIdentical(coverage(gr, shift = list(0, 1, 2)),
-                   RleList("chr1" = Rle(1:3, c(4, 1, 5)),
-                           "chr2" = Rle(0:3, c(2, 1, 1, 7)),
-                           "chr3" = Rle(0:4, c(8, 1, 1, 1, 1)),
-                           compress=FALSE))
+
+    target <- RleList(chr1=Rle(1:3, c(4, 1, 5)),
+                      chr2=Rle(0:3, c(1, 1, 1, 7)),
+                      chr3=Rle(0:4, c(6, 1, 1, 1, 1)),
+                      compress=FALSE)
+    checkIdentical(target, coverage(gr))
+
+    width <- c(chr1=10, chr2=20, chr3=30)
+    target <- RleList(chr1=Rle(1:3, c(4, 1, 5)),
+                      chr2=Rle(c(0:3, 0L), c(1, 1, 1, 7, 10)),
+                      chr3=Rle(c(0:4, 0L), c(6, 1, 1, 1, 1, 20)),
+                      compress=FALSE)
+    checkIdentical(target, coverage(gr, width=width))
+
+    weight <- list(chr1=1L, chr2=10L, chr3=100L)
+    target <- RleList(chr1=Rle(1:3, c(4, 1, 5)),
+                      chr2=Rle(10L * 0:3, c(1, 1, 1, 7)),
+                      chr3=Rle(100L * 0:4, c(6, 1, 1, 1, 1)),
+                      compress=FALSE)
+    checkIdentical(target, coverage(gr, weight=weight))
+
+    shift <- list(chr1=0, chr2=1, chr3=2)
+    target <- RleList(chr1=Rle(1:3, c(4, 1, 5)),
+                      chr2=Rle(0:3, c(2, 1, 1, 7)),
+                      chr3=Rle(0:4, c(8, 1, 1, 1, 1)),
+                      compress=FALSE)
+    checkIdentical(target, coverage(gr, shift=shift))
 
     ## with circular sequences
     gr <- GRanges(seqnames=c("A", "B"),
@@ -54,41 +61,48 @@ test_coverage_GRanges <- function() {
     gr@seqinfo <- Seqinfo(seqnames=c("A", "B"),
                           seqlengths=c(10, NA),
                           isCircular=c(TRUE, FALSE))
-    checkIdentical(coverage(gr),
-                   RleList(A=Rle(c(1L, 0L, 1L), c(1, 3, 6)),
-                           B=Rle(c(0L, 1L), c(5, 7)),
-                           compress=FALSE))
+    target <- RleList(A=Rle(c(1L, 0L, 1L), c(1, 3, 6)),
+                      B=Rle(c(0L, 1L), c(5, 7)),
+                      compress=FALSE)
+    checkIdentical(target, coverage(gr))
 }
 
 test_coverage_GRangesList <- function() {
     grl <- make_test_GRangesList()
-    checkIdentical(coverage(grl),
-                   RleList("chr1" = Rle(1:3, c(4, 1, 5)),
-                           "chr2" = Rle(c(1L, 3L, 5L, 6L, 3L), c(1, 1, 1, 7, 3)),
-                           "chr3" = Rle(0:4, c(6, 1, 1, 1, 1)),
-                           "chr4" = Rle(0:6, c(3, 1, 1, 1, 1, 1, 5)),
-                           "chr5" = Rle(0:4, c(9, 1, 1, 1, 1)),
-                           compress=FALSE))
-    checkIdentical(coverage(grl, width = c(10, 20, 30, 40, 50)),
-                   RleList("chr1" = Rle(1:3, c(4, 1, 5)),
-                           "chr2" = Rle(c(1L, 3L, 5L, 6L, 3L, 0L), c(1, 1, 1, 7, 3, 7)),
-                           "chr3" = Rle(c(0:4, 0L), c(6, 1, 1, 1, 1, 20)),
-                           "chr4" = Rle(c(0:6, 0L), c(3, 1, 1, 1, 1, 1, 5, 27)),
-                           "chr5" = Rle(c(0:4, 0L), c(9, 1, 1, 1, 1, 37)),
-                           compress=FALSE))
-    checkIdentical(coverage(grl, weight = list(1L, 10L, 100L, 1000L, 10000L)),
-                   RleList("chr1" = Rle(1:3, c(4, 1, 5)),
-                           "chr2" = Rle(10L * c(1L, 3L, 5L, 6L, 3L), c(1, 1, 1, 7, 3)),
-                           "chr3" = Rle(100L * 0:4, c(6, 1, 1, 1, 1)),
-                           "chr4" = Rle(1000L * 0:6, c(3, 1, 1, 1, 1, 1, 5)),
-                           "chr5" = Rle(10000L * 0:4, c(9, 1, 1, 1, 1)),
-                           compress=FALSE))
-    checkIdentical(coverage(grl, shift = list(0, 1, 2, 3, 4)),
-                   RleList("chr1" = Rle(1:3, c(4, 1, 5)),
-                           "chr2" = Rle(c(0L, 1L, 3L, 5L, 6L, 3L), c(1, 1, 1, 1, 7, 3)),
-                           "chr3" = Rle(0:4, c(8, 1, 1, 1, 1)),
-                           "chr4" = Rle(0:6, c(6, 1, 1, 1, 1, 1, 5)),
-                           "chr5" = Rle(0:4, c(13, 1, 1, 1, 1)),
-                           compress=FALSE))
+
+    target <- RleList(chr1=Rle(1:3, c(4, 1, 5)),
+                      chr2=Rle(c(1L, 3L, 5L, 6L, 3L), c(1, 1, 1, 7, 3)),
+                      chr3=Rle(0:4, c(6, 1, 1, 1, 1)),
+                      chr4=Rle(0:6, c(3, 1, 1, 1, 1, 1, 5)),
+                      chr5=Rle(0:4, c(9, 1, 1, 1, 1)),
+                      compress=FALSE)
+    checkIdentical(target, coverage(grl))
+
+    width <- c(chr1=10, chr2=20, chr3=30, chr4=40, chr5=50)
+    target <- RleList(chr1=Rle(1:3, c(4, 1, 5)),
+                      chr2=Rle(c(1L, 3L, 5L, 6L, 3L, 0L), c(1, 1, 1, 7, 3, 7)),
+                      chr3=Rle(c(0:4, 0L), c(6, 1, 1, 1, 1, 20)),
+                      chr4=Rle(c(0:6, 0L), c(3, 1, 1, 1, 1, 1, 5, 27)),
+                      chr5=Rle(c(0:4, 0L), c(9, 1, 1, 1, 1, 37)),
+                      compress=FALSE)
+    checkIdentical(target, coverage(grl, width=width))
+
+    weight <- list(chr1=1L, chr2=10L, chr3=100L, chr4=1000L, chr5=10000L)
+    target <- RleList(chr1=Rle(1:3, c(4, 1, 5)),
+                      chr2=Rle(10L * c(1L, 3L, 5L, 6L, 3L), c(1, 1, 1, 7, 3)),
+                      chr3=Rle(100L * 0:4, c(6, 1, 1, 1, 1)),
+                      chr4=Rle(1000L * 0:6, c(3, 1, 1, 1, 1, 1, 5)),
+                      chr5=Rle(10000L * 0:4, c(9, 1, 1, 1, 1)),
+                      compress=FALSE)
+    checkIdentical(target, coverage(grl, weight=weight))
+
+    shift <- list(chr1=0, chr2=1, chr3=2, chr4=3, chr5=4)
+    target <- RleList(chr1=Rle(1:3, c(4, 1, 5)),
+                      chr2=Rle(c(0L, 1L, 3L, 5L, 6L, 3L), c(1, 1, 1, 1, 7, 3)),
+                      chr3=Rle(0:4, c(8, 1, 1, 1, 1)),
+                      chr4=Rle(0:6, c(6, 1, 1, 1, 1, 1, 5)),
+                      chr5=Rle(0:4, c(13, 1, 1, 1, 1)),
+                      compress=FALSE)
+    checkIdentical(target, coverage(grl, shift=shift))
 }
 
