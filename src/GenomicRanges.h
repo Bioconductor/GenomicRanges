354c0a7a4883e0ef3c55b2b7b7b7bb7cfdfe18f5
Herve Pages <hpages@fhcrc.org>
2013-12-06 07:56:15 +0000

    Move low-level CIGAR utilities to GenomicAlignments package.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GenomicRanges@83970 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/GenomicRanges.h
+++ src/GenomicRanges.h
@@ -1,59 +1,6 @@
 #include <Rdefines.h>
 
 
-/* cigar_utils.c */
-
-SEXP valid_cigar(SEXP cigar, SEXP ans_type);
-
-SEXP explode_cigar_ops(
-	SEXP cigar,
-	SEXP ops
-);
-
-SEXP explode_cigar_op_lengths(
-	SEXP cigar,
-	SEXP ops
-);
-
-SEXP cigar_op_table(SEXP cigar);
-
-SEXP cigar_ranges(
-	SEXP cigar,
-	SEXP flag,
-	SEXP space,
-	SEXP pos,
-	SEXP f,
-	SEXP ops,
-	SEXP drop_empty_ranges,
-	SEXP reduce_ranges,
-	SEXP with_ops
-);
-
-SEXP cigar_width(
-	SEXP cigar,
-	SEXP flag,
-	SEXP space
-);
-
-SEXP cigar_narrow(SEXP cigar, SEXP left_width, SEXP right_width);
-
-SEXP cigar_qnarrow(SEXP cigar, SEXP left_qwidth, SEXP right_qwidth);
-
-SEXP ref_locs_to_query_locs(
-        SEXP ref_locs,
-        SEXP cigar,
-        SEXP pos,
-        SEXP narrow_left
-);
-
-SEXP query_locs_to_ref_locs(
-        SEXP query_locs,
-        SEXP cigar,
-        SEXP pos,
-        SEXP narrow_left
-);
-
-
 /* transcript_utils.c */
 
 SEXP transcript_widths(
--- src/R_init_GenomicRanges.c
+++ src/R_init_GenomicRanges.c
@@ -5,18 +5,6 @@
 
 static const R_CallMethodDef callMethods[] = {
 
-/* cigar_utils.c */
-	CALLMETHOD_DEF(valid_cigar, 2),
-	CALLMETHOD_DEF(explode_cigar_ops, 2),
-	CALLMETHOD_DEF(explode_cigar_op_lengths, 2),
-	CALLMETHOD_DEF(cigar_op_table, 1),
-	CALLMETHOD_DEF(cigar_ranges, 9),
-	CALLMETHOD_DEF(cigar_width, 3),
-	CALLMETHOD_DEF(cigar_narrow, 3),
-	CALLMETHOD_DEF(cigar_qnarrow, 3),
-	CALLMETHOD_DEF(ref_locs_to_query_locs, 4),
-	CALLMETHOD_DEF(query_locs_to_ref_locs, 4),
-
 /* transcript_utils.c */
 	CALLMETHOD_DEF(transcript_widths, 2),
 	CALLMETHOD_DEF(tlocs2rlocs, 5),
