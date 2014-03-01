49ce7f6b72791a676470f21cd117b20a0e83b0af
Herve Pages <hpages@fhcrc.org>
2014-03-01 00:40:14 +0000

    edit some TODO comments
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GenomicRanges@86948 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/range-squeezers.R
+++ R/range-squeezers.R
@@ -13,7 +13,10 @@ setGeneric("grglist", signature="x",
 )
 
 ### Extract the ranges as a RangesList object.
-### TODO: This one should probably be in IRanges.
+### TODO: This one should probably be in IRanges together with ranges(), which
+### is another range-squeezer.
+### TODO: For consistency the ranges() generic should also get the 'use.mcols'
+### arg with default to FALSE.
 setGeneric("rglist", signature="x",
     function(x, use.mcols=FALSE, ...) standardGeneric("rglist")
 )
