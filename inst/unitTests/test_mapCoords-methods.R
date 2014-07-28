Bioconductor/GenomicRanges

commit cb6081e9de94c7c2d8c3b63e3be4da82e5a529ca

add 'ignore.strand' and 'eltHits' args to mapCoords,GenomicRanges,GRangesList-method
git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GenomicRanges@92445 bc3139a8-67e5-0310-9ffc-ced21a209358

DESCRIPTION
@@ -7,7 +7,7 @@ Description: The ability to efficiently represent and manipulate genomic
 	intervals. Specialized containers for representing and manipulating
 	short alignments against a reference genome are defined in the
 	GenomicAlignments package.
-Version: 1.17.22
+Version: 1.17.23
 Author: P. Aboyoun, H. Pages and M. Lawrence
 Maintainer: Bioconductor Package Maintainer <maintainer@bioconductor.org>
 biocViews: Genetics, Infrastructure, Sequencing, Annotation, Coverage, 

R/mapCoords-methods.R
@@ -35,29 +35,39 @@ setMethod("map", c("GenomicRanges", "GRangesList"), function(from, to) {
   .Deprecated(msg="map() has been deprecated. Use mapCoords() instead.")
 })
 
-setMethod("mapCoords", c("GenomicRanges", "GRangesList"), function(x, to, ...) {
-  ## make sure 'to' is properly sorted by strand
-  to <- .orderElementsByTranscription(to)
+setMethod("mapCoords", c("GenomicRanges", "GRangesList"), 
+  function(x, to, ..., ignore.strand = FALSE, eltHits = FALSE) {
+    ## make sure 'to' is properly sorted by strand
+    to <- .orderElementsByTranscription(to)
 
-  ## find overlaps
-  gr <- unlist(to, use.names=FALSE)
-  ol <- findOverlaps(x, gr, type = "within")
-  shits <- subjectHits(ol)
-  qhits <- queryHits(ol)
-  local <- ranges(x)[qhits]
-  bounds <- ranges(gr)[shits]
+    ## find overlaps
+    gr <- unlist(to, use.names = FALSE)
+    ol <- findOverlaps(x, gr, type = "within", ..., ignore.strand=ignore.strand)
+    shits <- subjectHits(ol)
+    qhits <- queryHits(ol)
+    local <- ranges(x)[qhits]
+    bounds <- ranges(gr)[shits]
 
-  ## location wrt start of coding region 
-  neg <- as.vector(strand(gr)[shits] == "-")
-  local[!neg] <- shift(local[!neg], - start(bounds)[!neg])
-  local[neg] <- IRanges(end(bounds)[neg] - end(local)[neg],
-                        width = width(local)[neg])
+    ## location wrt start of individual list elements
+    if (ignore.strand) { 
+      local <- shift(local, - start(bounds))
+    } else {
+      neg <- as.vector(strand(gr)[shits] == "-")
+      local[!neg] <- shift(local[!neg], - start(bounds)[!neg])
+      local[neg] <- IRanges(end(bounds)[neg] - end(local)[neg],
+                            width = width(local)[neg])
+    }
 
-  ## location wrt transcript
-  cumsums <- .listCumsumShifted(width(to))
-  local <- shift(local, 1L + cumsums[shits])
+    ## location wrt transcript
+    cumsums <- .listCumsumShifted(width(to))
+    local <- shift(local, 1L + cumsums[shits])
 
-  toInd <- togroup(to)[shits]
-  GRanges(seqnames(gr)[shits], local, strand = strand(gr[shits]),
-          queryHits = qhits, subjectHits = toInd)
-})
+    toInd <- togroup(to)[shits]
+    if (eltHits)
+      GRanges(seqnames(gr)[shits], local, strand = strand(gr[shits]),
+              queryHits = qhits, subjectHits = toInd, eltHits = shits)
+    else
+      GRanges(seqnames(gr)[shits], local, strand = strand(gr[shits]),
+              queryHits = qhits, subjectHits = toInd)
+  }
+)

man/mapCoords-methods.Rd
@@ -18,15 +18,24 @@
 }
 
 \usage{
-\S4method{mapCoords}{GenomicRanges,GRangesList}(x, to, ...)
+\S4method{mapCoords}{GenomicRanges,GRangesList}(x, to, ..., 
+          ignore.strand = FALSE, eltHits = FALSE)
 }
 
 \arguments{
-  \item{x}{The input ranges to map, usually
-    a \code{\linkS4class{GenomicRanges}}}
+  \item{x}{The input ranges to map, usually a \code{\linkS4class{GRanges}}.}
  
   \item{to}{The alignment between the sequences in \code{x} and the
     sequences in the result.}
+
+  \item{ignore.strand}{Indicates if strand should be ignored in
+    overlap operations.}
+
+  \item{eltHits}{Indicates if element indices of the GRangesList
+    hit in the overlap operation should be returned as a metadata 
+    column. The `subjectHits` metadata column (always returned)
+    is an index for the outer list elements; `eltHits` is and 
+    index for the inner list elements.}
 
   \item{\dots}{Arguments passed to other methods.}
 }
@@ -35,8 +44,8 @@
   A \code{GRanges} object of mapped coordinates with matching data
   as metadata columns (`queryHits` and `subjectHits`).
 
-  The matching data is the result of calling \code{findOverlaps} with
-  type `within` on  ranges in \code{x} (the query) to the 
+  Matching data are the result of calling \code{findOverlaps} with
+  type `within` on  ranges in \code{x} (the query) and the 
   ranges in \code{to} (the subject). Matching can be many-to-one 
   or one-to-many; one row is reported for each match. 
 }

commit 0d3501c1b81846ed6fb693189461a4ac9e03ba6e

fix bug in mapCoords,GenomicRanges,GRangesList-method introduced in r92445
git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GenomicRanges@92923 bc3139a8-67e5-0310-9ffc-ced21a209358

DESCRIPTION
@@ -7,7 +7,7 @@ Description: The ability to efficiently represent and manipulate genomic
 	intervals. Specialized containers for representing and manipulating
 	short alignments against a reference genome are defined in the
 	GenomicAlignments package.
-Version: 1.17.27
+Version: 1.17.28
 Author: P. Aboyoun, H. Pages and M. Lawrence
 Maintainer: Bioconductor Package Maintainer <maintainer@bioconductor.org>
 biocViews: Genetics, Infrastructure, Sequencing, Annotation, Coverage, 

R/mapCoords-methods.R
@@ -42,22 +42,20 @@ setMethod("mapCoords", c("GenomicRanges", "GRangesList"),
 
     ## find overlaps
     gr <- unlist(to, use.names = FALSE)
-    ol <- findOverlaps(x, gr, type = "within", ..., ignore.strand=ignore.strand)
+    ol <- findOverlaps(x, gr, type = "within", ..., 
+                       ignore.strand=ignore.strand)
     shits <- subjectHits(ol)
     qhits <- queryHits(ol)
     local <- ranges(x)[qhits]
     bounds <- ranges(gr)[shits]
 
     ## location wrt start of individual list elements
-    if (ignore.strand) { 
-      local <- shift(local, - start(bounds))
-    } else {
-      neg <- as.vector(strand(gr)[shits] == "-")
-      local[!neg] <- shift(local[!neg], - start(bounds)[!neg])
-      local[neg] <- IRanges(end(bounds)[neg] - end(local)[neg],
-                            width = width(local)[neg])
-    }
+    neg <- as.vector(strand(gr)[shits] == "-")
+    local[!neg] <- shift(local[!neg], - start(bounds)[!neg])
+    local[neg] <- IRanges(end(bounds)[neg] - end(local)[neg],
+                          width = width(local)[neg])
 
+    toInd <- togroup(to)[shits]
     ## location wrt transcript
     cumsums <- .listCumsumShifted(width(to))
     local <- shift(local, 1L + cumsums[shits])

inst/unitTests/test_mapCoords-methods.R
@@ -13,3 +13,19 @@ test_mapCoords <- function()
     checkIdentical(start(x), c(645L, 609L, 1167L))
     checkTrue(all(width(x) == 200L)) 
 }
+
+test_mapCoords_strand <- function()
+{
+    gr <- GRanges("chrA", IRanges(c(43522349, 43522349),
+                   width=c(1, 1)), strand=c("+", "-"))
+    grl <- GRangesList(
+               GRanges("chrA", IRanges(43528406, 43528644), strand="-"),
+               GRanges("chrA", IRanges(43522244, 43524145), strand="-"))
+    map <- mapCoords(gr, grl, ignore.strand=FALSE)
+    checkIdentical(start(map) == 1797L)
+    checkIdentical(end(map) == 1797L)
+
+    map <- mapCoords(gr, grl, ignore.strand=TRUE)
+    checkIdentical(start(map) == c(1797L, 1797L))
+    checkIdentical(end(map) == c(1797L, 1797L))
+}