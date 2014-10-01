040b76ef0954b95a9b4ebfa467e27cab352ddc64
Michael Lawrence <lawremi@iastate.edu>
2014-10-01 22:58:22 +0000

    go ahead and add the DelegatingGenomicRanges class; did not mean to check-in the DESCRIPTION but I guess the cat is out of the bag
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GenomicRanges@94827 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/DelegatingGenomicRanges-class.R
@@ -0,0 +1,24 @@
+### =========================================================================
+### DelegatingGenomicRanges objects
+### -------------------------------------------------------------------------
+###
+### Virtual class that delegates GenomicRanges data access to a
+### GenomicRanges delegate.
+###
+
+setClass("DelegatingGenomicRanges",
+         representation(delegate="GenomicRanges"),
+         contains=c("GenomicRanges", "VIRTUAL"))
+
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+### Slot getters and setters.
+###
+
+setMethod("seqnames", "DelegatingGenomicRanges",
+          function(x) seqnames(x@delegate))
+
+setMethod("ranges", "DelegatingGenomicRanges",
+          function(x, ...) ranges(x@delegate, ...))
+
+setMethod("strand", "DelegatingGenomicRanges", function(x) strand(x@delegate))
+setMethod("seqinfo", "DelegatingGenomicRanges", function(x) seqinfo(x@delegate))
