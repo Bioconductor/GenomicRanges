58f1cbc0d8f8551ef598f00972ede8537d81c985
Valerie Obenchain <vobencha@fhcrc.org>
2015-01-01 01:35:38 +0000

    add coordinate mapping methods
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GenomicRanges@97947 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/coordinate-mapping-methods.R
+++ R/coordinate-mapping-methods.R
@@ -1,10 +1,42 @@
 ### =========================================================================
-### 'mapToGenome' and 'mapToTranscript' methods
+### [p]mapToGenome() and [p]mapToTranscript() methods
 ### -------------------------------------------------------------------------
 ###
 
+### I. Mapping strategy
+
+### genome -> transcript:
+### The *ToTranscript methods use findOverlaps for mapping. mapToTranscript 
+### method attempts to map all elements of 'x' to all elements of 'alignment'
+### where the parallel method only maps the i-th element of 'x' with the
+### i-th element of 'alignment'.
+###
+### - findOverlaps handles strand
+### - shift coords manually when 'alignment' is GRangesList
+###
+### transcript -> genome:
+### The *ToGenome methods use the C code beneath transcriptLocs2refLocs()
+### for mapping. mapToGenome does not attempt to map all i-j combinations but
+### instead uses a name match between 'x' and 'alignment' to determine
+### mapping pairs. The parallel pmapToGenome only attempts to map the i-th 
+### element of 'x' with the i-th element of 'alignment'.
+###
+### - check strand manually
+### - tlocs2rlocs handles shifting of coords when 'alignment' is GRangesList
+
+
+### II. Output format
+
+### Parallel methods return an object the same shape as 'x'. Ranges with
+### a strand mismatch or non-hit are returned as zero-width ranges.
+###
+### Non-parallel methods return an object that varies in length like a
+### Hits object. The result only contains mapped records, strand mismatch
+### and non-hits are not returned.
+
+
 ### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-### Generics
+### Generics 
 ###
 
 setGeneric("mapToGenome", signature=c("x", "alignment"),
@@ -31,8 +63,10 @@ setGeneric("pmapToTranscript", signature=c("x", "alignment"),
 ### Helpers
 ###
 
-### This method differs from sort() in that negative strand
-### elements are returned highest value to lowest.
+### 'x' is a GRangesList
+### Returns a GRangesList with sorted elements.
+### The method differs from sort() in that negative strand elements are
+### returned highest value to lowest.
 .orderElementsByTranscription <- function(x, ignore.strand) {
     original <- unlist(sapply(elementLengths(x), function(xx) 1:xx), 
                        use.names=FALSE)
@@ -48,7 +82,6 @@ setGeneric("pmapToTranscript", signature=c("x", "alignment"),
     if (ignore.strand) {
         ord <- S4Vectors:::mseq(pstart, pend)
     } else {
-        ## order by strand 
         neg <- strand(gr)[pstart] == "-"
         ord <- S4Vectors:::mseq(ifelse(neg, pend, pstart),
                                 ifelse(neg, pstart, pend))
@@ -59,12 +92,12 @@ setGeneric("pmapToTranscript", signature=c("x", "alignment"),
 }
 
 ### 'x' is an IntegerList or NumericList
-### returns a numeric vector of cumulative sums within list elements
+### Returns a numeric vector of cumulative sums within list elements.
 .listCumsumShifted <- function(x) {
-  cs <- unlist(cumsum(x), use.names=FALSE)
-  shifted <- c(0L, head(cs, -1))
-  shifted[start(PartitioningByWidth(elementLengths(x)))] <- 0L
-  shifted
+    cs <- unlist(cumsum(x), use.names=FALSE)
+    shifted <- c(0L, head(cs, -1))
+    shifted[start(PartitioningByWidth(elementLengths(x)))] <- 0L
+    shifted
 }
 
 
@@ -76,25 +109,40 @@ setGeneric("pmapToTranscript", signature=c("x", "alignment"),
     if (length(hits)) {
         alignmentHits <- subjectHits(hits)
         xHits <- queryHits(hits)
-        position <- ranges(x)[xHits]
+        xrange <- ranges(x)[xHits]
         bounds <- ranges(alignment)[alignmentHits]
 
         ## location wrt to start of individual list elements
         if (ignore.strand) {
-            position <- shift(position, - start(bounds))
+            xrange <- shift(xrange, - start(bounds) + 1L)
         } else {
             neg <- as.vector(strand(alignment)[alignmentHits] == "-")
-            position[!neg] <- shift(position[!neg], - start(bounds)[!neg])
-            position[neg] <- IRanges(end(bounds)[neg] - end(position)[neg],
-                                        width=width(position)[neg])
+            negstart <- end(bounds)[neg] - end(xrange)[neg] + 1L
+            xrange[neg] <- IRanges(negstart, width=width(xrange)[neg])
+            xrange[!neg] <- shift(xrange, - start(bounds) + 1L)
         }
-        ans <- GRanges(seqnames(x)[xHits], position, strand(x)[xHits],
-                       DataFrame(xHits, alignmentHits))
+        GRanges(seqnames(x)[xHits], xrange, strand(x)[xHits],
+                DataFrame(xHits, alignmentHits))
     } else {
         ans <- GRanges()
         mcols(ans) <- DataFrame(xHits=integer(), alignmentHits=integer())
+        ans
     }
-    ans
+}
+
+.pmapToTranscript <- function(x, alignment, hits, ignore.strand) { 
+    starts <- rep(1L, length(x))
+    ends <- rep(0L, length(x))
+    map <- .mapToTranscript(x, alignment, hits, ignore.strand)
+    if (length(map)) {
+        xHits <- mcols(map)$xHits
+        starts[xHits] <- start(map)
+        ends[xHits] <- end(map)
+    }
+    ## zero-width seqname
+    seqname <- as.character(seqnames(x))
+    seqname[setdiff(seq_along(x), queryHits(hits))] <- "unmapped"
+    GRanges(seqname, IRanges(starts, ends, names=names(x)), strand(x))
 }
 
 setMethod("mapToTranscript", c("GenomicRanges", "GenomicRanges"), 
@@ -119,33 +167,18 @@ setMethod("mapToTranscript", c("GenomicRanges", "GRangesList"),
         alignment <- .orderElementsByTranscription(alignment, ignore.strand)
         hits <- findOverlaps(x, unlist(alignment, use.names=FALSE), 
                              type="within", ignore.strand=ignore.strand)
-        map <- .mapToTranscript(x, unlist(alignment, use.names=FALSE), hits,
-                                ignore.strand)
+        map <- .mapToTranscript(x, unlist(alignment, use.names=FALSE), 
+                                hits, ignore.strand)
 
         ## location wrt start of concatenated list elements
         if (length(map)) {
             shifted <- .listCumsumShifted(width(alignment))
-            map <- shift(map, 1L + shifted[subjectHits(hits)])
+            map <- shift(map, shifted[subjectHits(hits)])
             mcols(map)$alignmentHits <- togroup(alignment)[subjectHits(hits)]
         }
         map
     }
 )
-
-.pmapToTranscript <- function(x, alignment, hits, ignore.strand=TRUE) { 
-    ## FIXME: handle non-hits re: pintersect arg 'resolve.empty'
-    starts <- rep(1L, length(x))
-    ends <- rep(0L, length(x))
-    map <- .mapToTranscript(x, alignment, hits, ignore.strand)
-    if (length(map)) {
-        xHits <- mcols(map)$xHits
-        starts[xHits] <- start(map)
-        ends[xHits] <- end(map)
-    }
-    ans <- GRanges(seqnames(x), IRanges(starts, ends), strand(x))
-    names(ans) <- names(x)
-    ans
-}
 
 setMethod("pmapToTranscript", c("GenomicRanges", "GenomicRanges"), 
     function(x, alignment, ignore.strand=TRUE, ...) 
@@ -153,7 +186,6 @@ setMethod("pmapToTranscript", c("GenomicRanges", "GenomicRanges"),
         if (length(x) != length(alignment))
             stop("'x' and 'alignment' must have the same length")
 
-        ## FIXME: pfindOverlaps?
         hits <- findOverlaps(x, alignment, type="within", 
                              ignore.strand=ignore.strand)
         ith_hits <- queryHits(hits) == subjectHits(hits)
@@ -167,30 +199,29 @@ setMethod("pmapToTranscript", c("GenomicRanges", "GRangesList"),
     {
         if (length(x) != length(alignment))
             stop("'x' and 'alignment' must have the same length")
-        strand <- strand(x)
         if (!ignore.strand)
             if (!all(elementLengths(runLength(strand(alignment))) == 1))
                 stop(paste0("when ignore.strand=TRUE all inner list ",
                             "elements of 'alignments' must be the same strand"))
 
-        ## order within list elements by strand
+        ## order within list elements
         alignment <- .orderElementsByTranscription(alignment, ignore.strand)
-        ## FIXME: pfindOverlaps?
         hits <- findOverlaps(x, unlist(alignment, use.names=FALSE), 
                              type="within", ignore.strand=ignore.strand)
+        ## filter hits on outer list element index
         ith_hits <- queryHits(hits) == togroup(alignment)[subjectHits(hits)]
         hits <- hits[ith_hits] 
-        map <- .pmapToTranscript(x, unlist(alignment, use.names=FALSE), 
+        ans <- .pmapToTranscript(x, unlist(alignment, use.names=FALSE), 
                                  hits, ignore.strand)
 
         ## location wrt start of concatenated list elements
         ## shift zero-width ranges only
-        if (any(hasWidth <- width(map) != 0L)) {
+        if (any(hasWidth <- width(ans) != 0L)) {
             shifted <- .listCumsumShifted(width(alignment))
-            map[hasWidth] <- shift(map[hasWidth], 
-                                   1L + shifted[subjectHits(hits)])
+            ans[hasWidth] <- shift(ans[hasWidth], shifted[subjectHits(hits)])
         }
-        map
+
+        ans 
     }
 )
 
@@ -198,4 +229,132 @@ setMethod("pmapToTranscript", c("GenomicRanges", "GRangesList"),
 ### mapToGenome() and pmapToGenome() methods
 ###
 
+.mapToGenome <- function(x, alignment, hits, ignore.strand)
+{
+    xHits <- queryHits(hits)
+    alignmentHits <- subjectHits(hits)
+    ## strand mismatch
+    if (!ignore.strand) {
+        strand_x <- strand(x)[xHits]
+        strandAlign <- unlist(runValue(strand(alignment)), 
+                              use.names=FALSE)[alignmentHits]
+        strandMismatch <- 
+            as.logical(strand_x == "+" & strandAlign == "-" |
+                       strand_x == "-" & strandAlign == "+")
+        strand <- as.character(strandAlign)[alignmentHits]
+    } else {
+        strandMismatch <- logical(length(xHits))
+        strand <- rep("+", length(xHits))
+    }
+    xStart <- as.list(start(x)[xHits])
+    xEnd <- as.list(end(x)[xHits])
+    alignStart <- as.list(start(alignment)[alignmentHits])
+    alignEnd <- as.list(end(alignment)[alignmentHits])
 
+    starts <- unlist(.Call("tlocs2rlocs", xStart, alignStart, 
+                           alignEnd, strand, FALSE, FALSE),
+                     use.names=FALSE) 
+    ends <- unlist(.Call("tlocs2rlocs", xEnd, alignStart, 
+                         alignEnd, strand, FALSE, FALSE), 
+                   use.names=FALSE) 
+
+    seqname <- as.character(seqnames(x)[xHits])
+    if (any(skip <- is.na(starts) | is.na(ends) | strandMismatch)) {
+        starts[skip] <- 1L 
+        ends[skip] <- 0L
+        seqname[skip] <- "unmapped"
+    }
+
+    GRanges(Rle(seqname), IRanges(starts, ends, names=names(x)[xHits]),
+            strand=strand)
+}
+
+setMethod("pmapToGenome", c("GenomicRanges", "GRangesList"), 
+    function(x, alignment, ignore.strand=TRUE, ...) 
+    {
+        if (length(x) && length(alignment)) {
+            if (length(x) != length(alignment))
+                stop("'x' and 'alignment' must have the same length")
+            if (!ignore.strand)
+                if (!all(elementLengths(runLength(strand(alignment))) == 1))
+                    stop(paste0("when ignore.strand=TRUE all inner list ",
+                                "elements of 'alignments' must have the ",
+                                "same strand"))
+
+            ## order within list elements
+            alignment <- .orderElementsByTranscription(alignment, ignore.strand)
+            hits <- Hits(seq_along(x), seq_along(x), length(x), length(x))
+            .mapToGenome(x, alignment, hits, ignore.strand)
+        } else GRanges()
+    }
+)
+
+setMethod("pmapToGenome", c("GenomicRanges", "GenomicRanges"),
+    function(x, alignment, ignore.strand=TRUE, ...)
+        pmapToGenome(x, splitAsList(alignment, seq_along(alignment)), 
+                     ignore.strand=ignore.strand, ...)
+)
+
+setMethod("pmapToGenome", c("Ranges", "GenomicRanges"),
+    function(x, alignment, ...) { 
+        gr <- GRanges(seqnames(alignment), x)
+        hits <- Hits(seq_along(x), seq_along(x), length(x), length(x))
+        .mapToGenome(gr, alignment, hits, TRUE)
+    }
+)
+
+setMethod("mapToGenome", c("GenomicRanges", "GRangesList"), 
+    function(x, alignment, ignore.strand=TRUE, ...) 
+    {
+        if (length(x) && length(alignment)) {
+            if (is.null(xNames <- names(x)) || 
+                is.null(alignmentNames <- names(alignment)))
+                stop ("both 'x' and 'alignment' must have names")
+            if (!ignore.strand)
+                if (!all(elementLengths(runLength(strand(alignment))) == 1))
+                    stop(paste0("when ignore.strand=TRUE all inner list ",
+                                "elements of 'alignments' must have the ",
+                                "same strand"))
+
+            match0 <- match(alignmentNames, alignmentNames)
+            match1 <- match(xNames, alignmentNames)
+            group0 <- splitAsList(seq_along(alignmentNames), match0)
+            group1 <- group0[match(na.omit(match1), names(group0))]
+            xHits <- rep(which(!is.na(match1)), elementLengths(group1))
+            alignmentHits <- unlist(group1, use.names=FALSE)
+            if (!length(xHits <- na.omit(xHits)))
+                stop ("none of 'names(x)' are in 'names(alignment)'")
+
+            ## order within list elements
+            alignment <- .orderElementsByTranscription(alignment, ignore.strand)
+            hits <- Hits(xHits, alignmentHits, length(x), length(alignment))
+            ans <- .mapToGenome(x, alignment, hits, ignore.strand) 
+            ## remove zero-width ranges, add mcols 
+            df <- DataFrame(xHits, alignmentHits)
+            mcols(ans) <- df
+            if (any(hasWidth <- width(ans) != 0L))
+                ans <- ans[hasWidth]
+        } else {
+            ans <- GRanges()
+            mcols(ans) <- DataFrame(xHits=integer(), alignmentHits=integer())
+        }
+
+        ans
+    }
+)
+
+setMethod("mapToGenome", c("GenomicRanges", "GenomicRanges"),
+    function(x, alignment, ignore.strand=TRUE, ...) 
+    {
+        grl <- splitAsList(alignment, seq_along(alignment))
+        names(grl) <- names(alignment)
+        mapToGenome(x, grl, ignore.strand=ignore.strand, ...)
+    }
+)
+
+setMethod("mapToGenome", c("Ranges", "GenomicRanges"),
+    function(x, alignment, ...) { 
+        gr <- GRanges(seqnames(alignment), x)
+        ranges(mapToGenome(gr, alignment, TRUE, ...))
+    }
+)
--- inst/unitTests/test_coordinate-mapping-methods.R
+++ inst/unitTests/test_coordinate-mapping-methods.R
@@ -2,6 +2,9 @@ library(TxDb.Dmelanogaster.UCSC.dm3.ensGene)
 txdb <- TxDb.Dmelanogaster.UCSC.dm3.ensGene
 cdsbytx <- cdsBy(txdb, "tx")[1:3]
 
+
+### mapToTranscript and pmapToTranscript
+
 test_mapToTranscript <- function()
 {
     ## empty
@@ -77,8 +80,9 @@ test_pmapToTranscript <- function()
 
     ## length
     x <- GRanges("chr1", IRanges(1, width=1))
-    checkException(pmapToTranscript(x, GRanges()), silent=TRUE)
-    checkException(pmapToTranscript(x, GRangesList()), silent=TRUE)
+    y <- GRanges("chr1", IRanges(1:5, width=1))
+    checkException(pmapToTranscript(x, y), silent=TRUE)
+    checkException(pmapToTranscript(x, GRangesList(y, y)), silent=TRUE)
 
     ## strand
     x <- GRanges("chr1", IRanges(c(6, 16, 1), width=1, names=LETTERS[1:3]))
@@ -108,5 +112,100 @@ test_pmapToTranscript <- function()
     checkIdentical(width(ans), c(1L, 1L, 0L))
     checkIdentical(start(ans), c(5L, 5L, 1L))
 
+    ## out of bounds
+    x <- GRanges("chr1", IRanges(rep(40, 3), width=10))
+    align <- GRanges("chr1", IRanges(c(1, 50, 100), width=20))
+    ans <- pmapToTranscript(x, align) 
+    checkIdentical(seqnames(ans), Rle(as.factor("unmapped"), 3)) 
+}
+
+### mapToGenome and pmapToGenome
+
+test_mapToGenome <- function()
+{
+    ## empty
+    ans1 <- mapToGenome(GRanges(), GRanges())
+    ans2 <- mapToGenome(GRanges(), GRangesList())
+    checkTrue(length(ans1) == 0L)
+    checkTrue(length(ans2) == 0L)
+    checkIdentical(names(mcols(ans1)), c("xHits", "alignmentHits"))
+    checkIdentical(names(mcols(ans2)), c("xHits", "alignmentHits"))
+
+    ## strand
+    x <- GRanges("chr1", IRanges(rep(10, 5), width=2), strand="+")
+    names(x) <- c("A", "B", "C", "foo", "A")
+    gr <- GRanges("chr1", IRanges(c(10, 100), width=50), strand="+")
+    align = GRangesList(gr, gr, gr, gr)
+    names(align) <- c("C", "bar", "C", "A") 
+
+    ans <- mapToGenome(x, align)
+    checkTrue(length(ans) == 4L)
+    checkIdentical(names(ans), c("A", "C", "C", "A"))
+    checkTrue(all(width(ans) == 2L))
+    checkIdentical(mcols(ans)$xHits, c(1L, 3L, 3L, 5L))
+    checkIdentical(mcols(ans)$alignmentHits, c(4L, 1L, 3L, 4L))
+    ans <- mapToGenome(x, unlist(align, use.names=TRUE))
+    checkTrue(length(ans) == 8L)
+    checkIdentical(names(ans), c("A", "A", rep("C", 4), "A", "A"))
+    checkIdentical(mcols(ans)$alignmentHits, c(7L, 8L, 1L, 2L, 5L, 6L, 7L, 8L))
+
+    strand(align[[1]][1]) <- "-"
+    checkException(mapToGenome(x, align, ignore.strand=FALSE), silent=TRUE) 
+
+    strand(align[[1]]) <- "-"
+    ans <- mapToGenome(x, align, ignore.strand=FALSE) 
+    checkIdentical(mcols(ans)$alignmentHits, c(4L, 3L, 4L))
+    ans <- mapToGenome(x, align, ignore.strand=TRUE) 
+    checkIdentical(mcols(ans)$alignmentHits, c(4L, 1L, 3L, 4L))
+
     ## TODO: seqnames
+
+}
+
+test_pmapToGenome <- function()
+{
+    ## empty
+    ans1 <- pmapToGenome(GRanges(), GRanges())
+    ans2 <- pmapToGenome(GRanges(), GRangesList())
+    checkTrue(length(ans1) == 0L)
+    checkTrue(length(ans2) == 0L)
+    checkIdentical(names(mcols(ans1)), character(0))
+    checkIdentical(names(mcols(ans2)), character(0))
+
+    ## length
+    x <- GRanges("chr1", IRanges(1, width=1))
+    align <- GRanges("chr1", IRanges(1:5, width=1))
+    checkException(pmapToGenome(x, align), silent=TRUE)
+    checkException(pmapToGenome(x, GRangesList(align, align)), silent=TRUE)
+
+    ## strand
+    x <- GRanges("chr1", IRanges(c(1, 50, 150), width=1, names=LETTERS[1:3]))
+    gr <- GRanges("chr1", IRanges(c(100, 300), width=100))
+    align <- GRangesList(gr, gr, gr)
+
+    strand(x) <- "-"
+    strand(align) <- "+"
+    ans <- pmapToGenome(x, align, ignore.strand=FALSE)
+    checkTrue(length(x) == length(x))
+    checkTrue(all(width(ans) == 0L))
+    checkIdentical(names(ans), LETTERS[1:3])
+
+    strand(align[[2]][1]) <- "-"
+    checkException(pmapToGenome(x, align, ignore.strand=FALSE), silent=TRUE) 
+
+    strand(align) <- "+"
+    strand(x) <- "+"
+    ans <- pmapToGenome(x, align, ignore.strand=FALSE) 
+    checkIdentical(start(ans), c(100L, 149L, 349L))
+
+    strand(align) <- "-"
+    strand(x) <- "-"
+    ans <- pmapToGenome(x, align, ignore.strand=FALSE) 
+    checkIdentical(start(ans), c(399L, 350L, 150L))
+
+    ## out of bounds
+    x <- GRanges("chr1", IRanges(c(1, 50, 100), width=50))
+    align <- GRanges("chr1", IRanges(rep(40, 3), width=20))
+    ans <- pmapToGenome(x, align) 
+    checkIdentical(seqnames(ans), Rle(as.factor("unmapped"), 3)) 
 }
