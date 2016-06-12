c21c7e0b51ee4f92725423516e5db224838edd68
Michael Lawrence <lawremi@iastate.edu>
2016-06-12 15:50:50 +0000

    add more =>GenomicRanges coercions via GRanges
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GenomicRanges@118498 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/makeGRangesFromDataFrame.R
+++ R/makeGRangesFromDataFrame.R
@@ -241,8 +241,9 @@ makeGRangesFromDataFrame <- function(df,
 setAs("data.frame", "GRanges",
     function(from) makeGRangesFromDataFrame(from, keep.extra.columns=TRUE)
 )
+setAs("data.frame", "GenomicRanges", function(from) as(from, "GRanges"))
 
 setAs("DataFrame", "GRanges",
     function(from) makeGRangesFromDataFrame(from, keep.extra.columns=TRUE)
 )
-
+setAs("DataFrame", "GenomicRanges", function(from) as(from, "GRanges"))
