a786577c4c6e9179a3347db10b5025a70c175818
Hervé Pagès <hpages@fredhutch.org>
2018-02-07 22:44:51 -0800

    small tweak

--- R/GRanges-class.R
+++ R/GRanges-class.R
@@ -233,7 +233,7 @@ setMethod("updateObject", "GRanges",
             message("[updateObject] ", class(object), " object uses ",
                     "internal representation from GenomicRanges\n",
                     "[updateObject] ", version, ". Updating it ...")
-        object@elementType <- "GPos"
+        object@elementType <- new("GRanges")@elementType
         object
     }
 )
