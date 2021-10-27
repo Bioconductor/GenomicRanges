a524d931c2976b25c99dcfe3d02a18ed62deb67b
Marcel Ramos <LiNk-NY@users.noreply.github.com>
2021-10-27 09:46:31 -0400

    add na.rm argument to makeGRangesFromDataFrame (#59)
    * add na.rm argument to makeGRangesFromDataFrame
    
    * update logic; use paren
    
    * makeGRangesFromDataFrame na.rm updates
    
    * small tweaks and version bump
    
    * resolve version conflict
    
    * resolve version conflict (2nd attempt)
    
    Co-authored-by: Hervé Pagès <hpages.on.github@gmail.com>

--- R/makeGRangesFromDataFrame.R
+++ R/makeGRangesFromDataFrame.R
@@ -74,7 +74,7 @@
 .find_strand_col <- function(df_colnames, strand.field, prefix)
 {
     idx <- which(df_colnames %in% paste0(prefix, strand.field))
-    if (length(idx) == 0L) 
+    if (length(idx) == 0L)
         idx <- which(df_colnames %in% strand.field)
     if (length(idx) == 0L)
         return(NA_integer_)
@@ -156,7 +156,8 @@ makeGRangesFromDataFrame <- function(df,
                                      start.field="start",
                                      end.field=c("end", "stop"),
                                      strand.field="strand",
-                                     starts.in.df.are.0based=FALSE)
+                                     starts.in.df.are.0based=FALSE,
+                                     na.rm=FALSE)
 {
     ## Check args.
     if (is.character(df))  # for people that provide the path to a file
@@ -170,6 +171,8 @@ makeGRangesFromDataFrame <- function(df,
     ans_seqinfo <- normarg_seqinfo1(seqinfo)
     if (!isTRUEorFALSE(starts.in.df.are.0based))
         stop("'starts.in.df.are.0based' must be TRUE or FALSE")
+    if (!isTRUEorFALSE(na.rm))
+        stop(wmsg("'na.rm' must be TRUE or FALSE"))
 
     granges_cols <- .find_GRanges_cols(names(df),
                                        seqnames.field=seqnames.field,
@@ -178,12 +181,30 @@ makeGRangesFromDataFrame <- function(df,
                                        strand.field=strand.field,
                                        ignore.strand=ignore.strand)
 
-    ## Prepare 'ans_seqnames'.
-    ans_seqnames <- df[[granges_cols[["seqnames"]]]]
-
     ## Prepare 'ans_ranges'.
     ans_start <- .get_data_frame_col_as_numeric(df, granges_cols[["start"]])
     ans_end <- .get_data_frame_col_as_numeric(df, granges_cols[["end"]])
+    is_na <- is.na(ans_start) | is.na(ans_end)
+
+    if (any(is_na)) {
+        if (!na.rm) {
+            start_colname <- names(df)[[granges_cols[["start"]]]]
+            end_colname <- names(df)[[granges_cols[["end"]]]]
+            stop(wmsg(
+                "The \"", start_colname, "\" and/or \"", end_colname, "\" ",
+                "columns contain NAs. Use 'na.rm=TRUE' to ignore the rows ",
+                "with NAs."
+            ))
+        }
+        keep_idx <- which(!is_na)
+        df <- S4Vectors:::extract_data_frame_rows(df, keep_idx)
+        ans_start <- ans_start[keep_idx]
+        ans_end <- ans_end[keep_idx]
+    }
+
+    ## Prepare 'ans_seqnames'.
+    ans_seqnames <- df[[granges_cols[["seqnames"]]]]
+
     if (starts.in.df.are.0based)
         ans_start <- ans_start + 1L
     ans_names <- rownames(df)
